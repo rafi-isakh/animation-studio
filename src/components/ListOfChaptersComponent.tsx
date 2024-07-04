cde756e0d4947c6545e6815fbfcca29dd364c998
Jongmin Baek <jongminbaek@stelland.io>
2024-07-04 22:17:45 +0900

    Only hover:pink for link

--- src/components/ListOfChaptersComponent.tsx
+++ src/components/ListOfChaptersComponent.tsx
@@ -30,16 +30,16 @@ const ListOfChaptersComponent = ({ webnovel }: { webnovel: Webnovel }) => {
                         return (
                             chapters?.map((chapter) => (
                                 <tr className="bg-white">
-                                    <th scope="row" className="px-6 py-4 font-medium text-black whitespace-nowrap hover:text-pink-600">
+                                    <th scope="row" className="px-6 py-4 font-medium text-black whitespace-nowrap">
                                         <p>{chapter.id}</p>
                                     </th>
                                     <th scope="row" className="px-6 py-4 font-medium text-black whitespace-nowrap hover:text-pink-600">
                                         <Link href={`/chapter_view/${chapter.id}`} className="text-md">{chapter.title}</Link>
                                     </th>
-                                    <th scope="row" className="px-6 py-4 font-medium text-black whitespace-nowrap hover:text-pink-600">
+                                    <th scope="row" className="px-6 py-4 font-medium text-black whitespace-nowrap">
                                         <p>{chapter.created_at}</p>
                                     </th>
-                                    <th scope="row" className="px-6 py-4 font-medium text-black whitespace-nowrap hover:text-pink-600">
+                                    <th scope="row" className="px-6 py-4 font-medium text-black whitespace-nowrap">
                                         <p>{chapter.views}</p>
                                     </th>
                                 </tr>

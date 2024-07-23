d0ed5166f47ba9de81f4f7e7044a5d7752a455bc
Jongmin Baek <jongminbaek@stelland.io>
2024-07-23 16:23:12 +0900

    Change env var names for vercel policy

--- src/utils/s3.tsx
+++ src/utils/s3.tsx
@@ -2,12 +2,12 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 const REGION = process.env.NEXT_PUBLIC_AWS_REGION;
 const BUCKET_NAME = process.env.NEXT_PUBLIC_AWS_BUCKET_NAME;
-const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY ?? "";
-const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY ?? "";
+const AWS_S3_ACCESS_KEY = process.env.AWS_S3_ACCESS_KEY ?? "";
+const AWS_S3_SECRET_ACCESS_KEY = process.env.AWS_S3_SECRET_ACCESS_KEY ?? "";
 
 const credentials = {
-  accessKeyId: AWS_ACCESS_KEY,
-  secretAccessKey: AWS_SECRET_ACCESS_KEY
+  accessKeyId: AWS_S3_ACCESS_KEY,
+  secretAccessKey: AWS_S3_SECRET_ACCESS_KEY
 }
 
 const s3Client = new S3Client({

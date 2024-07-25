9bf9d7e9d6c1405a266277f20454355390e67b15
Jongmin Baek <jongminbaek@stelland.io>
2024-07-25 21:03:29 +0900

    Get rid of naver login for now

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -10,7 +10,6 @@ export default function SignIn() {
       <div className="flex flex-row space-x-4 m-4 justify-center" >
           <GoogleSignIn></GoogleSignIn>
           <KakaoSignIn></KakaoSignIn>
-          <NaverSignIn></NaverSignIn>
       </div>
     </div>
 

115c9f30a05a9bc21341fdf3fad7871ff6eedae4
Jongmin Baek <jongminbaek@stelland.io>
2024-07-27 01:10:01 +0900

    Add two more, also add keys

--- src/components/CarouselComponentReactSlick.tsx
+++ src/components/CarouselComponentReactSlick.tsx
@@ -17,11 +17,11 @@ import Link from 'next/link';
 const CarouselComponentReactSlick = ({ items }: { items: SlickCarouselItem[] }) => {
 
   const [key1, setKey1] = useState(0);
-  const [key2, setKey2] = useState(0);
-  const [key3, setKey3] = useState(0);
-  const [key4, setKey4] = useState(0);
-  const [key5, setKey5] = useState(0);
-  const [key6, setKey6] = useState(0);
+  const [key2, setKey2] = useState(2);
+  const [key3, setKey3] = useState(4);
+  const [key4, setKey4] = useState(6);
+  const [key5, setKey5] = useState(8);
+  const [key6, setKey6] = useState(10);
 
 
   const { language, dictionary } = useLanguage();
@@ -88,25 +88,25 @@ const CarouselComponentReactSlick = ({ items }: { items: SlickCarouselItem[] }) 
                 <div className="relative w-full h-auto aspect-[10/5] md:aspect-[1280/500] max-w-[1280px] mx-auto overflow-hidden"  >
                   {index == 0 &&
                     <div>
-                      <div className='absolute top-2 left-2 md:top-32 md:left-10 z-10 text-white md:text-4xl text-2xl font-bold w-[200px] md:w-[400px] flex flex-wrap'>
+                      <div key={key1} className='absolute top-2 left-2 md:top-32 md:left-10 z-10 text-white md:text-4xl text-2xl font-bold w-[200px] md:w-[400px] flex flex-wrap'>
                         <OtherTranslateComponent content={item.title} elementId={item.id.toString()} elementType={'carouselItem'} elementSubtype='title' /></div>
-                      <div className='absolute top-14 left-2 md:top-48 md:left-10 z-10 text-white md:text-lg text-xs w-[200px] md:w-[400px] flex flex-wrap'>
+                      <div key={key2} className='absolute top-14 left-2 md:top-48 md:left-10 z-10 text-white md:text-lg text-xs w-[200px] md:w-[400px] flex flex-wrap'>
                         <OtherTranslateComponent content={item.hook} elementId={item.id.toString()} elementType={'carouselItem'} elementSubtype='hook' /></div>
                     </div>}
                   {index == 1 &&
                     <div>
-                      <div className='absolute top-8 right-2 md:top-64 md:right-20 z-10 text-black md:text-4xl text-2xl font-bold w-[200px] md:w-[400px] flex flex-wrap'>
+                      <div key={key3} className='absolute top-8 right-2 md:top-64 md:right-20 z-10 text-black md:text-4xl text-2xl font-bold w-[200px] md:w-[400px] flex flex-wrap'>
                       <OtherTranslateComponent content={item.title} elementId={item.id.toString()} elementType={'carouselItem'} elementSubtype='title' /></div>
-                      <div className='absolute top-16 right-2 md:top-80 md:right-20 z-10 text-black md:text-lg text-sm w-[200px] md:w-[400px] flex flex-wrap'>
+                      <div key={key4} className='absolute top-16 right-2 md:top-80 md:right-20 z-10 text-black md:text-lg text-sm w-[200px] md:w-[400px] flex flex-wrap'>
                       <OtherTranslateComponent content={item.hook} elementId={item.id.toString()} elementType={'carouselItem'} elementSubtype='hook' /></div>
                       </div>}
                   {index == 2 &&
                     <div>
-                      <div className='absolute top-2 md:top-32 left-6 md:left-20 z-10 text-black md:text-4xl text-2xl font-bold w-[200px] md:w-[400px] flex flex-wrap'>
-                        {item.title}</div>
-                      <div className='absolute top-14 md:top-48 left-6 md:left-20 z-10 text-black md:text-lg text-sm w-[200px] md:w-[400px] flex flex-wrap'>
-                        {item.hook}</div>
-                    </div>}
+                      <div key={key5} className='absolute top-2 md:top-32 left-6 md:left-20 z-10 text-black md:text-4xl text-2xl font-bold w-[200px] md:w-[400px] flex flex-wrap'>
+                      <OtherTranslateComponent content={item.title} elementId={item.id.toString()} elementType={'carouselItem'} elementSubtype='title' /></div>
+                      <div key={key6} className='absolute top-14 md:top-48 left-6 md:left-20 z-10 text-black md:text-lg text-sm w-[200px] md:w-[400px] flex flex-wrap'>
+                      <OtherTranslateComponent content={item.hook} elementId={item.id.toString()} elementType={'carouselItem'} elementSubtype='hook' /></div>
+                      </div>}
                   <Image className="object-cover object-center w-full h-full rounded" src={getImageURL(item.image)} sizes="(max-width: 768px) 100vw, (max-width: 1280px) 90vw, 1280px" fill alt={item.description}
                     placeholder="blur" blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
                   />

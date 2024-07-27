2637ec930291cce4d475d337b0d6e684b0b3a211
Jongmin Baek <jongminbaek@stelland.io>
2024-07-28 00:55:28 +0900

    Add forgotten delete account route

---
+++ src/app/api/delete-account/route.ts
@@ -0,0 +1,35 @@
+import { auth } from "@/auth";
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(request: NextRequest) {
+    const session = await auth();
+    const { searchParams } = new URL(request.url);
+    const email = searchParams.get("email")
+  
+    if (!session || !session.user) {
+      return NextResponse.json({
+        "message": "Unauthorized!!",
+        "status": 401
+      });
+    }
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND}/api/delete_account?email=${email}`, {
+        headers: {
+            'Authorization': `Bearer ${session.accessToken}`
+          }
+    })
+    
+    if (!response.ok) {
+        console.error("Failed to delete account", email)
+        return NextResponse.json({
+            "message": "Failed to delete account",
+            "status": 500
+        })
+    }
+    
+    return NextResponse.json({
+        "message": "Deleted account",
+        "status": 200
+    })
+
+}

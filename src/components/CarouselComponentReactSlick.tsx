b01e0db0940df2e9ff83b898e6ca69d1d6f8c9d4
Jongmin Baek <jongminbaek@stelland.io>
2024-07-29 09:04:15 +0900

    Break-keep logic in carousel

--- src/components/CarouselComponentReactSlick.tsx
+++ src/components/CarouselComponentReactSlick.tsx
@@ -67,6 +67,15 @@ const CarouselComponentReactSlick = ({ items }: { items: SlickCarouselItem[] }) 
     return '#';
   }
 
+  function breakKeepOrNot() {
+    if (language == 'ko' || language == "ar" || language == "th" || language == "vi" || language == 'en' || language == 'id') {
+      return 'break-keep';
+    } else if (language == 'ja' || language == 'zh-CN' || language == 'zh-TW') {
+      return '';
+    }
+    return '';
+  }
+
   const settings = {
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -90,28 +99,28 @@ const CarouselComponentReactSlick = ({ items }: { items: SlickCarouselItem[] }) 
                   {index == 0 &&
                     <div className='relative'>
                       <div key={key1} className='absolute top-2 left-2 md:top-10 md:left-10 z-10 text-white md:text-4xl text-2xl font-bold w-[200px] md:w-[400px] flex flex-wrap'>
-                        <OtherTranslateComponent content={item.title} elementId={item.id.toString()} 
+                        <OtherTranslateComponent content={item.title} elementId={item.id.toString()} classParams={breakKeepOrNot()}
                         elementType={'carouselItem'} elementSubtype='title' showLoading={false} /></div>
                       <div key={key2} className='absolute top-10 left-2 md:top-[7rem] md:left-10 z-10 text-white md:text-lg text-sm w-[230px] md:w-[400px] flex flex-wrap'>
-                        <OtherTranslateComponent content={item.hook} elementId={item.id.toString()} 
+                        <OtherTranslateComponent content={item.hook} elementId={item.id.toString()} classParams={breakKeepOrNot()}
                         elementType={'carouselItem'} elementSubtype='hook' showLoading={false} /></div>
                     </div>}
                   {index == 1 &&
                     <div className="relative">
                       <div key={key3} className='absolute top-4 right-2 md:top-64 md:right-20 z-10 text-black md:text-4xl text-2xl font-bold w-[200px] md:w-[400px] flex flex-wrap'>
-                      <OtherTranslateComponent content={item.title} elementId={item.id.toString()} 
+                      <OtherTranslateComponent content={item.title} elementId={item.id.toString()} classParams={breakKeepOrNot()}
                       elementType={'carouselItem'} elementSubtype='title' showLoading={false} /></div>
                       <div key={key4} className='absolute top-16 right-2 md:top-80 md:right-20 z-10 text-black md:text-lg text-sm w-[200px] md:w-[400px] flex flex-wrap'>
-                      <OtherTranslateComponent content={item.hook} elementId={item.id.toString()} 
+                      <OtherTranslateComponent content={item.hook} elementId={item.id.toString()} classParams={breakKeepOrNot()}
                       elementType={'carouselItem'} elementSubtype='hook' showLoading={false}/></div>
                       </div>}
                   {index == 2 &&
                     <div className="relative">
                       <div key={key5} className='absolute top-2 md:top-10 left-6 md:left-10 z-10 text-black md:text-4xl text-2xl font-bold w-[200px] md:w-[400px] flex flex-wrap'>
-                      <OtherTranslateComponent content={item.title} elementId={item.id.toString()} 
+                      <OtherTranslateComponent content={item.title} elementId={item.id.toString()} classParams={breakKeepOrNot()}
                       elementType={'carouselItem'} elementSubtype='title' showLoading={false}/></div>
                       <div key={key6} className='absolute top-14 md:top-[7rem] left-6 md:left-10 z-10 text-black md:text-lg text-sm w-[135px] md:w-[400px] flex flex-wrap'>
-                      <OtherTranslateComponent content={item.hook} elementId={item.id.toString()} 
+                      <OtherTranslateComponent content={item.hook} elementId={item.id.toString()} classParams={breakKeepOrNot()}
                       elementType={'carouselItem'} elementSubtype='hook' showLoading={false}/></div>
                       </div>}
                   <Image className="object-cover object-center w-full h-full rounded" src={getImageURL(item.image)}  fill alt={item.description}

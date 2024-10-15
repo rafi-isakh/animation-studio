b3a7981ead5fe8b8341fec452e07dafaf3751d28
Dami <dami@stelland.io>
2024-10-15 15:27:10 +0900

    Feat: author profile styling.

--- src/components/WebnovelInfoAndPictureComponent.tsx
+++ src/components/WebnovelInfoAndPictureComponent.tsx
@@ -25,29 +25,73 @@ const WebNovelInfoAndPictureComponent = ({ webnovel }: { webnovel: Webnovel | un
 
     return webnovel && (
 
-        <div className='flex flex-row space-x-4 justify-between'>
-            <div className="flex flex-col space-y-4 w-full">
-                <p className="text-sm">{phrase(dictionary, webnovel.genre, language)}</p>
-                <OtherTranslateComponent key={key1} content={webnovel.title}
-                    elementId={webnovel.id.toString()} elementType='webnovel' elementSubtype="title" classParams="text-xl" />
-                <OtherTranslateComponent key={key2} content={webnovel.description}
-                    elementId={webnovel.id.toString()} elementType='webnovel' elementSubtype="description" />
-                <hr />
-                <Link href={`/view_profile/${webnovel.user.id}`}>
-                    <p className="text-sm font-bold hover:text-pink-600">{webnovel.user.nickname}</p>
-                </Link>
-                <p className="text-sm">{phrase(dictionary, "original", language)}: {phrase(dictionary, code_to_lang(webnovel.language), language)}</p>
-                <p className='mt-10 text-sm'><i className="fa-regular fa-heart"></i> {webnovel.upvotes}</p>
-                <p className='my-4 text-sm '><i className="fa-solid fa-eye"></i> {webnovel.views}</p>
-            </div>
-            <div className={`ml-4 ${isLoading ? 'animate-pulse' : ''}`}>
+        <div className='flex flex-col md:flex-row space-x-4 justify-between'>
+            {/* Novel Thumbnail */}
+            <div className={`md:mr-6 mr-0 md:self-start self-center ${isLoading ? 'animate-pulse' : ''}`}>
                 <Link href={firstChapter != -1 ? `/chapter_view/${firstChapter}` : "#"} className="text-md font-bold">
-                    <Image src={imageSrc} alt={webnovel?.title ?? "webnovel not found"} width={262} height={480}
-                        placeholder="blur" blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
+                    <Image 
+                        src={imageSrc} 
+                        alt={webnovel?.title ?? "webnovel not found"} 
+                        width={262} 
+                        height={480}
+                        className="mt-3 mb-6"
+                        placeholder="blur" 
+                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
                         onLoad={() => setIsLoading(false)}
                     />
                 </Link>
             </div>
+
+            <div className="flex flex-col space-y-4 w-full">
+              
+                <OtherTranslateComponent 
+                    key={key1} 
+                    content={webnovel.title}
+                    elementId={webnovel.id.toString()} 
+                    elementType='webnovel' 
+                    elementSubtype="title" 
+                    classParams="text-[2rem] uppercase"
+                    />
+
+                 {/* Author profile */}
+                  <Link href={`/view_profile/${webnovel.user.id}`}>
+                    <p className="text-sm font-bold hover:text-pink-600">
+                      <span className="text-[10px] self-center rounded-xl text-white bg-purple-500 px-2 py-1 mr-1"> 
+                      {phrase(dictionary, "author", language)}
+                      </span>     
+                        {webnovel.user.nickname}
+                    </p>
+                </Link>
+
+                <div className="flex flex-row space-x-4 items-center text-[12px] ">
+                    {/* Genre part */}
+                    <p className="uppercase">{phrase(dictionary, webnovel.genre, language)}</p>
+
+                    <p className="">
+                      <i className="fas fa-globe mr-1"></i>
+                      {/* {phrase(dictionary, "original", language)}:  */}
+                      {phrase(dictionary, code_to_lang(webnovel.language), language)}
+                    </p>
+                    <p className=' flex items-center'>
+                        <i className="fa-regular fa-heart mr-1"></i> {webnovel.upvotes}
+                    </p>
+                    <p className=' flex items-center'>
+                        <i className="fa-solid fa-eye mr-1"></i> {webnovel.views}
+                    </p>
+                    <p className=' flex items-center'>
+                        <i className="fa-solid fa-list mr-1"></i> 총 {webnovel.chapters.length} 화
+                    </p>
+                </div>
+                <OtherTranslateComponent 
+                    key={key2} 
+                    content={webnovel.description}
+                    elementId={webnovel.id.toString()} 
+                    elementType='webnovel' 
+                    elementSubtype="description" 
+                    />
+                <hr />
+            </div>
+
         </div>
     )
 }

496dc318c01281579dbe26df28cc99cccf49fb57
server <server@stelland.io>
2024-10-16 15:26:20 +0900

    Minor changes in studio

--- src/app/studio/novel/page.tsx
+++ src/app/studio/novel/page.tsx
@@ -22,7 +22,7 @@ export default function NovelStudioPage() {
     const [streamedSubCharacterSentence, setStreamedSubCharacterSentence] = useState("");
     const [streamedSynopsis, setStreamedSynopsis] = useState("");
     const [isGeneratingSynopsis, setIsGeneratingSynopsis] = useState(false);
-    const {language, dictionary} = useLanguage();
+    const { language, dictionary } = useLanguage();
 
 
     const processSSEResponse = async (response: Response, callback: (data: any) => void, setIsGenerating: (isGenerating: boolean) => void) => {
@@ -252,77 +252,77 @@ export default function NovelStudioPage() {
     return (
         <div className="md:w-[1280px] flex flex-col space-y-4 items-center justify-center mx-auto mb-24">
             <div className="w-[450px] sm:w-[720px] text-left pt-10">
-              <h1 className="font-bold">
-                {/* 세계관 설정 : Worldview setting */}
-                {phrase(dictionary, "worldviewSetting", language)}
-              </h1>
+                <h1 className="font-bold">
+                    {/* 세계관 설정 : Worldview setting */}
+                    {phrase(dictionary, "worldviewSetting", language)}
+                </h1>
             </div>
             <div className="border rounded-xl py-10 px-6 w-[450px] sm:w-[720px] mt-10">
-            <h1 className="font-bold mb-10">
-              {/* 장르 및 키워드 :  */}
-              {phrase(dictionary, "genresAndKeyword", language)}
-            </h1>
-            <div className="flex flex-col gap-4 ">
-                <label htmlFor="">
-                    {/* Genres */}
-                    {phrase(dictionary, "genre", language)}
-                </label>
-                <input 
-                className="rounded-md focus:ring-pink-600 focus:border-pink-600" 
-                type="text" 
-                value={genres} 
-                onChange={(e) => setGenres(e.target.value)} 
-                placeholder={phrase(dictionary, "genrePlaceholder", language)} /> 
+                <h1 className="font-bold mb-10">
+                    {/* 장르 및 키워드 :  */}
+                    {phrase(dictionary, "genresAndKeyword", language)}
+                </h1>
+                <div className="flex flex-col gap-4 ">
+                    <label htmlFor="">
+                        {/* Genres */}
+                        {phrase(dictionary, "genre", language)}
+                    </label>
+                    <input
+                        className="rounded-md focus:ring-pink-600 focus:border-pink-600"
+                        type="text"
+                        value={genres}
+                        onChange={(e) => setGenres(e.target.value)}
+                        placeholder={phrase(dictionary, "genrePlaceholder", language)} />
 
-                <label htmlFor="">
-                    {/* Keywords */}
-                    {phrase(dictionary, "keyword", language)}
-                </label>
-                <input 
-                className="rounded-md focus:ring-pink-600 focus:border-pink-600 " 
-                type="text" 
-                value={keywords} 
-                onChange={(e) => setKeywords(e.target.value)} 
-                placeholder={phrase(dictionary, "keywordPlaceholder", language)} />
-                <div className="flex flex-row justify-center space-y-3 pb-4">
-                <Button
-                    variant="outlined"
-                    color="gray"
-                    onClick={generateLogline}
-                    disabled={isGeneratingLogline}
-                    className="w-64 self-end font-bold border border-gray-600 ml-4 bg-white hover:text-pink-600 hover:border-pink-600"
-                >
-                    {isGeneratingLogline ? (<p>{phrase(dictionary, "generatingPrompt", language)}</p> ) 
-                                         : (<p>{phrase(dictionary, "generatingLogline", language)}</p> )
-                    }
-                    {/* writing icon */}
-                    <svg xmlns="http://www.w3.org/2000/svg" 
-                        width="18" 
-                        height="18" 
-                        viewBox="0 0 24 24" 
-                        fill="none" 
-                        stroke="currentColor" 
-                        stroke-width="2" 
-                        stroke-linecap="round" 
-                        stroke-linejoin="round" 
-                        className="lucide lucide-pencil-line ml-3 "
-                    >
-                    <path d="M12 20h9"/>
-                    <path d="M16.376 3.622a1 1 0 0 1 3.002 3.002L7.368 18.635a2 2 0 0 1-.855.506l-2.872.838a.5.5 0 0 1-.62-.62l.838-2.872a2 2 0 0 1 .506-.854z"/>
-                    <path d="m15 5 3 3"/>
-                    </svg>
+                    <label htmlFor="">
+                        {/* Keywords */}
+                        {phrase(dictionary, "keyword", language)}
+                    </label>
+                    <input
+                        className="rounded-md focus:ring-pink-600 focus:border-pink-600 "
+                        type="text"
+                        value={keywords}
+                        onChange={(e) => setKeywords(e.target.value)}
+                        placeholder={phrase(dictionary, "keywordPlaceholder", language)} />
+                    <div className="flex flex-row justify-center space-y-3 pb-4">
+                        <Button
+                            variant="outlined"
+                            color="gray"
+                            onClick={generateLogline}
+                            disabled={isGeneratingLogline}
+                            className="w-64 self-end font-bold border border-gray-600 ml-4 bg-white hover:text-pink-600 hover:border-pink-600"
+                        >
+                            {isGeneratingLogline ? (<p>{phrase(dictionary, "generatingPrompt", language)}</p>)
+                                : (<p>{phrase(dictionary, "generatingLogline", language)}</p>)
+                            }
+                            {/* writing icon */}
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                width="18"
+                                height="18"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                className="lucide lucide-pencil-line ml-3 "
+                            >
+                                <path d="M12 20h9" />
+                                <path d="M16.376 3.622a1 1 0 0 1 3.002 3.002L7.368 18.635a2 2 0 0 1-.855.506l-2.872.838a.5.5 0 0 1-.62-.62l.838-2.872a2 2 0 0 1 .506-.854z" />
+                                <path d="m15 5 3 3" />
+                            </svg>
 
-                 </Button>
+                        </Button>
 
-                 <Button
-                    variant="outlined"
-                    color="gray"
-                    onClick={generateAll}
-                    className="w-64 self-end font-bold border border-gray-600 ml-4 bg-white hover:text-pink-600 hover:border-pink-600"
-                     >　
-                     {phrase(dictionary, "generateAll", language)}
-                　</Button>
-                </div>
+                        <Button
+                            variant="outlined"
+                            color="gray"
+                            onClick={generateAll}
+                            className="w-64 self-end font-bold border border-gray-600 ml-4 bg-white hover:text-pink-600 hover:border-pink-600"
+                        >
+                            {phrase(dictionary, "generateAll", language)}
+                        </Button>
+                    </div>
 
                 </div>
             </div>
@@ -335,91 +335,91 @@ export default function NovelStudioPage() {
                 </h1>
             </div>
 
-             {/* Logline part */}
+            {/* Logline part */}
             <div className="bg-white rounded-xl border py-6 px-6 w-[450px] sm:w-[720px] mt-10 space-y-4">
 
-            <div className="flex flex-col gap-4 ">                
-                <div className="bg-gray-100 p-4 leading-loose">
-                    {streamedLogline}
-                </div>
+                <div className="flex flex-col gap-4 ">
+                    <div className="bg-gray-100 p-4 leading-loose">
+                        {streamedLogline}
+                    </div>
 
-                <Button 
-                variant="outlined" 
-                color="gray" 
-                onClick={() => generateMainCharacter(streamedLogline)} 
-                disabled={isGeneratingMainCharacter}
-                className=" self-center font-bold border border-gray-600 ml-4 bg-white hover:text-pink-600 hover:border-pink-600"
-                >
-                    {isGeneratingMainCharacter ? (<p>{phrase(dictionary, "generatingPrompt", language)}</p>) 
-                                               : (<p>{phrase(dictionary, "createMainCharacter", language)}</p>)}
+                    <Button
+                        variant="outlined"
+                        color="gray"
+                        onClick={() => generateMainCharacter(streamedLogline)}
+                        disabled={isGeneratingMainCharacter}
+                        className=" self-center font-bold border border-gray-600 ml-4 bg-white hover:text-pink-600 hover:border-pink-600"
+                    >
+                        {isGeneratingMainCharacter ? (<p>{phrase(dictionary, "generatingPrompt", language)}</p>)
+                            : (<p>{phrase(dictionary, "createMainCharacter", language)}</p>)}
 
-                    <svg 
-                            xmlns="http://www.w3.org/2000/svg" 
-                            width="18" 
-                            height="18" 
-                            viewBox="0 0 24 24" 
-                            fill="none" 
-                            stroke="currentColor" 
-                            stroke-width="2" 
-                            stroke-linecap="round" 
-                            stroke-linejoin="round" 
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="18"
+                            height="18"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
                             className="lucide lucide-brush ml-3"
                         >
-                          <path d="m9.06 11.9 8.07-8.06a2.85 2.85 0 1 1 4.03 4.03l-8.06 8.08"/>
-                          <path d="M7.07 14.94c-1.66 0-3 1.35-3 3.02 0 1.33-2.5 1.52-2 2.02 1.08 1.1 2.49 2.02 4 2.02 2.2 0 4-1.8 4-4.04a3.01 3.01 0 0 0-3-3.02z"/>
+                            <path d="m9.06 11.9 8.07-8.06a2.85 2.85 0 1 1 4.03 4.03l-8.06 8.08" />
+                            <path d="M7.07 14.94c-1.66 0-3 1.35-3 3.02 0 1.33-2.5 1.52-2 2.02 1.08 1.1 2.49 2.02 4 2.02 2.2 0 4-1.8 4-4.04a3.01 3.01 0 0 0-3-3.02z" />
                         </svg>
 
-                </Button>
-                <div className="">
+                    </Button>
+                    <div className="">
                         {isGeneratingMainCharacter ? (
                             <></>
                         ) : (
                             <CharacterInfo data={mainCharacter} title="주연 캐릭터" />
                         )}
+                    </div>
                 </div>
             </div>
-            </div>
-            
+
             <div className="w-[450px] sm:w-[720px] text-left pt-6">
                 <h1 className="font-bold">
                     {/* 캐릭터 설정 */}
                     <p>{phrase(dictionary, "characterSetting", language)}</p>
                 </h1>
             </div>
-        
+
             <div className="flex flex-col bg-white rounded-xl border py-6 px-6 w-[450px] sm:w-[720px] mt-10 space-y-4">
-                <Button 
-                    variant="outlined" 
-                    color="gray" 
-                    onClick={() => generateSubCharacter(mainCharacter, streamedLogline)} 
+                <Button
+                    variant="outlined"
+                    color="gray"
+                    onClick={() => generateSubCharacter(mainCharacter, streamedLogline)}
                     disabled={isGeneratingSubCharacter}
                     className="self-center font-bold border border-gray-600 ml-4 bg-white hover:text-pink-600 hover:border-pink-600"
+                >
+                    {isGeneratingSubCharacter ? (<p>{phrase(dictionary, "generatingPrompt", language)}</p>)
+                        : (<p>{phrase(dictionary, "createSubCharacter", language)}</p>)}
+
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="18"
+                        height="18"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        className="lucide lucide-brush ml-3"
                     >
-                        {isGeneratingSubCharacter ? (<p>{phrase(dictionary, "generatingPrompt", language)}</p>) 
-                                                  : (<p>{phrase(dictionary, "createSubCharacter", language)}</p>)}
+                        <path d="m9.06 11.9 8.07-8.06a2.85 2.85 0 1 1 4.03 4.03l-8.06 8.08" />
+                        <path d="M7.07 14.94c-1.66 0-3 1.35-3 3.02 0 1.33-2.5 1.52-2 2.02 1.08 1.1 2.49 2.02 4 2.02 2.2 0 4-1.8 4-4.04a3.01 3.01 0 0 0-3-3.02z" />
+                    </svg>
 
-                        <svg 
-                            xmlns="http://www.w3.org/2000/svg" 
-                            width="18" 
-                            height="18" 
-                            viewBox="0 0 24 24" 
-                            fill="none" 
-                            stroke="currentColor" 
-                            stroke-width="2" 
-                            stroke-linecap="round" 
-                            stroke-linejoin="round" 
-                            className="lucide lucide-brush ml-3"
-                        >
-                          <path d="m9.06 11.9 8.07-8.06a2.85 2.85 0 1 1 4.03 4.03l-8.06 8.08"/>
-                          <path d="M7.07 14.94c-1.66 0-3 1.35-3 3.02 0 1.33-2.5 1.52-2 2.02 1.08 1.1 2.49 2.02 4 2.02 2.2 0 4-1.8 4-4.04a3.01 3.01 0 0 0-3-3.02z"/>
-                        </svg>
+                </Button>
 
-                    </Button>
+                <div className="">
+                    {subCharacter && <CharacterInfo data={subCharacter} title="조연 캐릭터" />}
 
-                    <div className="">
-                        {subCharacter && <CharacterInfo data={subCharacter} title="조연 캐릭터" />}
-                    
-                     </div>
+                </div>
             </div>
 
 
@@ -432,8 +432,8 @@ export default function NovelStudioPage() {
 
 
             <div className="flex flex-col justify-center bg-white rounded-xl border py-6 px-6 w-[450px] sm:w-[720px] mt-10 space-y-4">
-                
-               {/* <div className="bg-gray-100 p-4 leading-loose">
+
+                {/* <div className="bg-gray-100 p-4 leading-loose">
                     {streamedMainCharacterSentence}
                 </div> 
                         
@@ -470,15 +470,15 @@ export default function NovelStudioPage() {
                 </div>
 
 
-                <Button 
-                    variant="outlined" 
-                    color="gray" 
-                    onClick={() => generateEpisodeConfig(mainCharacter, subCharacter, streamedLogline)} 
+                <Button
+                    variant="outlined"
+                    color="gray"
+                    onClick={() => generateEpisodeConfig(mainCharacter, subCharacter, streamedLogline)}
                     disabled={isGeneratingEpisodeConfig}
                     className="self-center font-bold border border-gray-600 bg-white hover:text-pink-600 hover:border-pink-600"
                 >
-                    {isGeneratingEpisodeConfig ? (<p>{phrase(dictionary, "generatingPrompt", language)}</p>) 
-                                               : (<p>{phrase(dictionary, "createEpisode", language)}</p>)}
+                    {isGeneratingEpisodeConfig ? (<p>{phrase(dictionary, "generatingPrompt", language)}</p>)
+                        : (<p>{phrase(dictionary, "createEpisode", language)}</p>)}
                 </Button>
 
 
@@ -486,18 +486,18 @@ export default function NovelStudioPage() {
                     {streamedSynopsis}
                 </div>
 
-                <Button 
-                variant="outlined" 
-                color="gray" 
-                onClick={() => generateSynopsis(mainCharacter, subCharacter, streamedLogline, streamedEpisodeConfig)} 
-                disabled={isGeneratingSynopsis}
-                className="self-center font-bold border border-gray-600 bg-white hover:text-pink-600 hover:border-pink-600"
+                <Button
+                    variant="outlined"
+                    color="gray"
+                    onClick={() => generateSynopsis(mainCharacter, subCharacter, streamedLogline, streamedEpisodeConfig)}
+                    disabled={isGeneratingSynopsis}
+                    className="self-center font-bold border border-gray-600 bg-white hover:text-pink-600 hover:border-pink-600"
                 >
-                    {isGeneratingSynopsis ? (<p>{phrase(dictionary, "generatingPrompt", language)}</p> ) 
-                                          : (<p>{phrase(dictionary, "createSynopsis", language)}</p>)}
+                    {isGeneratingSynopsis ? (<p>{phrase(dictionary, "generatingPrompt", language)}</p>)
+                        : (<p>{phrase(dictionary, "createSynopsis", language)}</p>)}
                 </Button>
 
-             
+
 
             </div>
         </div>

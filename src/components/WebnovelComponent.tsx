7386bb448f477057e1210e8089a2f597a1987813
server <server@stelland.io>
2024-10-17 23:12:59 +0900

    Fix text width of webnovel cards

--- src/components/WebnovelComponent.tsx
+++ src/components/WebnovelComponent.tsx
@@ -29,7 +29,7 @@ const WebnovelComponent = ({ webnovel, index, ranking }: { webnovel: Webnovel, i
     return (
         <Link href={`/view_webnovels?id=${webnovel.id}`}>
             <Card className='p-2 rounded-xl'>
-                <div className="overflow-hidden flex items-center w-[244px] h-[116px] md:w-[325px] md:h-[155px]">
+                <div className="overflow-hidden flex items-center w-full h-[116px] md:w-[325px] md:h-[155px]">
                     <div className="flex flex-row mt-2 space-x-4 items-center">
                         <Image src={imageSrc} width={imageWidth} height={imageHeight} alt={webnovel.cover_art} className="rounded-xl"
                             placeholder="blur" blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg==" // 추가
@@ -37,7 +37,7 @@ const WebnovelComponent = ({ webnovel, index, ranking }: { webnovel: Webnovel, i
                         {ranking && <p className={`text-3xl md:text-2xl`}>{index + 1}</p>}
                         <div className="mt-1">
                             <OtherTranslateComponent key={key} content={webnovel.title}
-                                elementId={webnovel.id.toString()} elementType='webnovel' elementSubtype="title" classParams="text-md md:text-lg max-w-32 md:max-w-32 lg:max-w-48 break-words" />
+                                elementId={webnovel.id.toString()} elementType='webnovel' elementSubtype="title" classParams="text-md md:text-lg w-64 md:max-w-32 lg:max-w-48 break-words" />
                             <p className="text-xs md:text-sm font-bold">{webnovel.user.nickname}</p>
                         </div>
                     </div>

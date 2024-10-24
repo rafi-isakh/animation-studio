99ac94c4f9b94d65d40027b4ec8e000195374f5b
Dami <dami@stelland.io>
2024-10-24 09:16:57 +0900

    top banner resizing by mobile & desktop screen

--- src/components/ApplyCreatorBanner.tsx
+++ src/components/ApplyCreatorBanner.tsx
@@ -7,36 +7,65 @@ const ApplyCreatorBanner = () => {
     const { dictionary, language } = useLanguage();
 
     return (
-        <div className='bg-black w-full h-[3rem] mb-4'>
+        <div className='bg-black w-full h-[3rem] mb-4 mt-2'> 
+        {/*  <div className='flex justify-center self-center'>  */}
         <Link href='/signin'>
         {language == 'ko' ? (
+                            <>
                             <Image
                                 src='/apply_creator_banner_KR.svg' 
                                 alt='Toonyz event banner'
                                 sizes="cover"
                                 width={0}
                                 height={0}
-                                className='relative md:-top-[19px] top-1 mx-auto'
+                                className='relative md:-top-[19px] top-1 mx-auto md:block lg:block hidden hover:opacity-[0.8]'
                                 style={{
                                     width: '1280px',
                                     height: 'auto'
                                 }}
                                 />
+                            <Image
+                                src='/apply_creator_banner_mobile_KR.svg' 
+                                alt='Toonyz event banner'
+                                sizes="cover"
+                                width={0}
+                                height={0}
+                                className='relative md:-top-[19px] -top-[18px] mx-auto md:hidden lg:hidden hover:opacity-[0.8]'
+                                style={{
+                                    width: '450px',
+                                    height: 'auto'
+                                }}
+                                />
+                             </>
                                ) 
                              : (
+                                <>
                                 <Image
                                 src='/apply_creator_banner_EN.svg' 
                                 alt='Toonyz event banner'
                                 sizes="cover"
                                 width={0}
                                 height={0}
-                                className='relative md:-top-[19px] top-1 mx-auto'
+                                className='relative md:-top-[19px] top-1 mx-auto md:block lg:block hidden hover:opacity-[0.8]'
                                 style={{
                                     width: '1280px',
                                     height: 'auto'
                                 }}
                             />
-                          )
+                                <Image
+                                src='/apply_creator_banner_mobile_EN.svg' 
+                                alt='Toonyz event banner'
+                                sizes="cover"
+                                width={0}
+                                height={0}
+                                className='relative md:-top-[19px] -top-[18px] mx-auto md:hidden lg:hidden hover:opacity-[0.8]'
+                                style={{
+                                    width: '450px',
+                                    height: 'auto'
+                                }}
+                                />
+                            </>
+                     )
                  }
             </Link>
         </div>

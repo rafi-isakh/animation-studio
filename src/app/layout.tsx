2c44946edd64fe218d655d86ca0cbfbadd19a8bf
server <server@stelland.io>
2024-10-28 15:11:12 +0900

    Layout bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,7 @@ import { LanguageProvider } from '@/contexts/LanguageContext';
 import { AuthProvider } from '@/contexts/AuthContext';
 import { DeviceProvider } from '@/contexts/DeviceContext';
 import { Metadata } from 'next'
-import { Oleo_Script_Swash_Caps } from 'next/font/google'
 import { Noto_Sans, Noto_Sans_KR, Noto_Sans_Arabic, Noto_Sans_Thai, Noto_Sans_JP, Noto_Sans_TC, Noto_Sans_SC } from 'next/font/google'
-import Footer from '@/components/Footer';
-import ChargePointsTemporary from '@/components/ChargePointsTemporary';
 import Margin from '@/components/Margin';
 
 

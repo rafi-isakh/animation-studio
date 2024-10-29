f20f3017b1aa3f9023d9ddc414f262495917a499
server <server@stelland.io>
2024-10-29 10:50:33 +0900

    Swap out cloudfront distribution

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '3000',
-                pathname: '/api/images/**',
-            },
-        ],
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: `${process.env.NEXT_PUBLIC_CLOUDFRONT}`,
-                pathname: '/**',
-            },
-        ],
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: process.env.NEXT_PUBLIC_CLOUDFRONT,
+            pathname: '/**',
+        }, ],
     },
     async headers() {
-        return [
-            {
-                source: '/upload/(.*)',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=31536000, immutable',
-                    },
-                ],
-            },
-        ];
+        return [{
+            source: '/upload/(.*)',
+            headers: [{
+                key: 'Cache-Control',
+                value: 'public, max-age=31536000, immutable',
+            }, ],
+        }, ];
     },
 };
 

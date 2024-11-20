db1cd3b8501d757ff05ebb4da601d2d2cf5cc4e5
Dami <dami@stelland.io>
2024-11-20 09:06:06 +0900

    fix: Removed the Link tag comment

--- src/components/Billboard.tsx
+++ src/components/Billboard.tsx
@@ -43,29 +43,6 @@ const Billboard = () => {
             {phrase(dictionary, "Billboard_subheader", language)}
         </p>
         <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
-         
-              {/* <Link 
-               className="
-                bg-white
-                text-white
-                dark:text-white
-                  bg-opacity-30 
-                  rounded-md 
-                  py-1 md:py-2 
-                  px-2 md:px-4
-                  w-auto 
-                  text-xs lg:text-lg 
-                  font-semibold
-                  flex
-                  flex-row
-                  items-center
-                  hover:bg-opacity-20
-                  transition" 
-                href='/'>
-                <Info className="w-4 md:w-7 mr-1" />
-                More Info
-              </Link> */}
-       
         </div>
       </div>
     </div>

b5663bcb3d64bebae03b016c5fe5277e0ad3bc7f
server <server@stelland.io>
2024-12-13 18:02:25 +0900

    Webtoon footer, fix bug where files starting with . were displaying x boxees

--- src/app/webtoons/[slug]/[episode]/page.tsx
+++ src/app/webtoons/[slug]/[episode]/page.tsx
@@ -1,12 +1,24 @@
 import ViewWebtoonComponent from "@/components/ViewWebtoonComponent";
 import WebtoonViewerFooter from "@/components/WebtoonViewerFooter";
 
+async function getWebtoonById(id: string) {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/get_webtoon_by_id?id=${id}`,
+        {
+            cache: "no-store"
+        }
+    )
+    if (!response.ok) {
+        throw new Error(`Failed to fetch webtoon with id ${id}`)
+    }
+    return await response.json()
+}
 
 export default async function WebtoonEpisodePage({ params }: { params: { slug: string, episode: string } }) {
+    const webtoon = await getWebtoonById(params.slug)
     return (
         <div>
-            <ViewWebtoonComponent webtoonId={params.slug} episode={params.episode} />
-            <WebtoonViewerFooter webtoonId={params.slug} episode={params.episode} />
+            <ViewWebtoonComponent webtoon={webtoon} episode={params.episode} />
+            <WebtoonViewerFooter webtoon={webtoon} episode={params.episode} />
         </div>
     )
 }
--- src/components/WebtoonViewerFooter.tsx
+++ src/components/WebtoonViewerFooter.tsx
@@ -1,41 +1,115 @@
+"use client"
 import Link from "next/link"
-import { WebtoonChapter } from "./Types"
+import { Webtoon, WebtoonChapter } from "./Types"
+import DictionaryPhrase from "./DictionaryPhrase"
+import { useState, useEffect } from "react"
+import { useModalStyle } from "@/styles/ModalStyles"
+import { Box } from "@mui/material"
+import { Button } from "@mui/material"
+import { Modal } from "@mui/material"
 
-const getWebtoonById = async (id: string) => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/get_webtoon_by_id?id=${id}`,
-        {
-            cache: "no-store"
-        }
-    )
-    return await response.json()
-}
+export default function WebtoonViewerFooter({ webtoon, episode }: { webtoon: Webtoon, episode: string }) {
+    const [isVisible, setIsVisible] = useState(true)
+    const [lastScrollY, setLastScrollY] = useState(0)
+    const [showIsLastChapterModal, setShowIsLastChapterModal] = useState(false)
+    const [showIsFirstChapterModal, setShowIsFirstChapterModal] = useState(false)
 
-export default async function WebtoonViewerFooter(props: { webtoonId: string, episode: string }) {
-    const webtoon = await getWebtoonById(props.webtoonId)
+    const sortedChapters = [...webtoon.chapters].sort((a: WebtoonChapter, b: WebtoonChapter) => a.directory.localeCompare(b.directory))
+    useEffect(() => {
+        let timeoutId: NodeJS.Timeout;
+
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+
+            if (currentScrollY < lastScrollY) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+
+            setLastScrollY(currentScrollY);
+
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                setIsVisible(false); // Hide after a delay when scrolling stops
+            }, 2000);
+        };
+        window.addEventListener('scroll', handleScroll);
+
+        // Cleanup function to remove the event listener
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+            clearTimeout(timeoutId); // Clear timeout when component unmounts
+        };
+    }, [lastScrollY]); // Dependency array to trigger when lastScrollY changes
 
     const getNextChapterId = (currentChapterId: string) => {
-        const index = webtoon.chapters.findIndex((ch: WebtoonChapter) => ch.directory === currentChapterId);
-        if (index === webtoon.chapters.length - 1) {
+        const index = sortedChapters.findIndex((ch: WebtoonChapter) => ch.directory === currentChapterId);
+        if (index === sortedChapters.length - 1) {
             return currentChapterId; // Stay on the same chapter if it's the last one
         }
-        return webtoon.chapters[index + 1].directory;
+        return sortedChapters[index + 1].directory;
     }
 
     const getPrevChapterId = (currentChapterId: string) => {
-        const index = webtoon.chapters.findIndex((ch: WebtoonChapter) => ch.directory === currentChapterId);
+        const index = sortedChapters.findIndex((ch: WebtoonChapter) => ch.directory === currentChapterId);
         if (index === 0) {
             return currentChapterId; // Stay on the same chapter if it's the first one
         }
-        return webtoon.chapters[index - 1].directory;
+        return sortedChapters[index - 1].directory;
     }
 
+    const handlePrevChapter = () => {
+        if (episode === sortedChapters[0].directory) {
+            setShowIsFirstChapterModal(true);
+        }
+    }
+
+    const handleNextChapter = () => {
+        if (episode === sortedChapters[sortedChapters.length - 1].directory) {
+            setShowIsLastChapterModal(true);
+        }
+    }
+
+    const prevChapterLink = `/webtoons/${webtoon.id}/${getPrevChapterId(episode)}`
+    const nextChapterLink = `/webtoons/${webtoon.id}/${getNextChapterId(episode)}`
+
     return (
-        <div className="z-50 fixed w-full justify-center bg-white text-black dark:text-black border-t bottom-0 left-2 pt-2 pb-2 right-2 md:mr-0 mr-[15px] md:ml-0 transition-transform duration-300">
-            <div className="max-w-lg text-black dark:text-black flex flex-wrap items-center justify-evenly mx-auto p-2">
-                <Link href={`/webtoons/${props.webtoonId}/${getPrevChapterId(props.episode)}`}>Prev</Link>
-                <Link href={`/webtoons/${props.webtoonId}/${getNextChapterId(props.episode)}`}>Next</Link>
+        <>
+            <div className={`z-50 fixed w-full justify-center bg-white text-black dark:text-black border-t bottom-0 left-2 pt-2 pb-2 right-2 md:mr-0 mr-[15px] md:ml-0 transition-transform duration-300"
+            ${isVisible ? 'translate-y-0' : 'translate-y-full'}`}>
+                <div className="max-w-lg text-black dark:text-black flex flex-wrap items-center justify-evenly mx-auto p-2">
+                    <Link href={prevChapterLink} onClick={handlePrevChapter}>
+                        <p className='group hover:text-pink-600'>
+                            <i className="fas fa-angle-left  text-gray-500 self-center group-hover:text-pink-600 mr-4"></i>
+                            {<DictionaryPhrase phraseVar="prevChapter" />}
+                        </p>
+                    </Link>
+                    <Link href={nextChapterLink} onClick={handleNextChapter}>
+                        <p className='group hover:text-pink-600'>
+                            {<DictionaryPhrase phraseVar="nextChapter" />}
+                            <i className="fas fa-angle-right  text-gray-500 self-center group-hover:text-pink-600 ml-4"></i>
+                        </p>
+                    </Link>
+                </div>
             </div>
-        </div>
+            <Modal open={showIsLastChapterModal} onClose={() => setShowIsLastChapterModal(false)}>
+                <Box sx={useModalStyle}>
+                    <div className='flex flex-col space-y-4'>
+                        <p><DictionaryPhrase phraseVar="isLastChapter" /></p>
+                        <Button color="gray" variant="outlined" onClick={() => setShowIsLastChapterModal(false)}><DictionaryPhrase phraseVar="ok" /></Button>
+                    </div>
+                </Box>
+            </Modal>
+            <Modal open={showIsFirstChapterModal} onClose={() => setShowIsFirstChapterModal(false)}>
+                <Box sx={useModalStyle}>
+                    <div className='flex flex-col space-y-4'>
+                        <p><DictionaryPhrase phraseVar="isFirstChapter" /></p>
+                        <Button color="gray" variant="outlined" onClick={() => setShowIsFirstChapterModal(false)}><DictionaryPhrase phraseVar="ok" /></Button>
+                    </div>
+                </Box>
+            </Modal>
+        </>
     )
 
 }

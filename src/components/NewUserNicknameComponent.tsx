c524c783dd355da63ac1c76e8aa7e3866f5c9686
Dami <dami@stelland.io>
2024-12-17 10:33:45 +0900

    fix: changed the text color to black in the sign up forms when it's on the darkmode.

--- src/components/NewUserNicknameComponent.tsx
+++ src/components/NewUserNicknameComponent.tsx
@@ -2,18 +2,19 @@
 
 import { useLanguage } from "@/contexts/LanguageContext"
 import { phrase } from '@/utils/phrases';
+import '@/styles/new_user.css'
 
 const NewUserNicknameComponent = () => {
     const { dictionary, language } = useLanguage();
 
     return (
-        <div className='w-full'>
+        <div className='w-full text-black dark:text-black'>
             <input
                 placeholder={phrase(dictionary, "nickname", language)}
                 type="text"
                 name="nickname"
                 required
-                className='input rounded-xl w-full border border-gray-300'
+                className='input rounded-xl w-full border border-gray-300 text-black dark:text-black'
             />
         </div>
     )

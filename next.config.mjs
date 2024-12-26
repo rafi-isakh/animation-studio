217f3129081260f83ab814e86a95f0fc2ab57ebb
server <server@stelland.io>
2024-12-26 23:05:55 +0900

    Service worker

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,28 @@
 /** @type {import('next').NextConfig} */
+import withPWA from 'next-pwa';
+
+const pwaConfig = withPWA({
+    dest: 'public', // Where the service worker and cache files will be output
+    register: true, // Automatically registers the service worker
+    skipWaiting: true, // Ensures updated service worker activates immediately
+});
+
 const nextConfig = {
     webpack: (config) => {
         // Add rule for Lottie JSON files
         config.module.rules.push({
-          test: /\.lottie$/,
-          type: 'asset/source',
-          use: {
-            loader: 'lottie-loader',
-          },
+            test: /\.lottie$/,
+            type: 'asset/source',
+            use: {
+                loader: 'lottie-loader',
+            },
         });
         return config;
-      },
+    },
     images: {
-        remotePatterns: [
-            {
+        remotePatterns: [{
                 protocol: 'https',
-                hostname: process.env.NEXT_PUBLIC_PICTURES_S3  || 'localhost',
+                hostname: process.env.NEXT_PUBLIC_PICTURES_S3 || 'localhost',
                 pathname: '/**',
             },
             {
@@ -36,9 +43,9 @@ const nextConfig = {
             headers: [{
                 key: 'Cache-Control',
                 value: 'public, max-age=31536000, immutable',
-            },],
-        },];
+            }, ],
+        }, ];
     },
 };
 
-export default nextConfig;
+export default pwaConfig({...nextConfig });

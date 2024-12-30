35518d5c6558f953c92ad87b67e62f397b553267
server <server@stelland.io>
2024-12-30 10:39:55 +0900

    Dont use useEffect in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,17 @@
 // app/layout.tsx
 import '@/styles/globals.css';
 import { Metadata } from 'next'
+import { DeviceProvider } from '@/contexts/DeviceContext';
+import { ThemeProvider } from '@/contexts/providers';
+import { LanguageProvider } from '@/contexts/LanguageContext';
+import '@/styles/globals.css';
+import { ReactNode, Suspense, useEffect } from 'react';
+import { AuthProvider } from '@/contexts/AuthContext';
+import { UserProvider } from '@/contexts/UserContext';
+import Header from '@/components/Header';
+import { SearchProvider } from '@/contexts/SearchContext';
+import Margin from '@/components/Margin';
+import { Noto_Sans, Noto_Sans_Thai, Noto_Sans_KR, Noto_Sans_TC, Noto_Sans_JP, Noto_Sans_SC, Noto_Sans_Arabic } from 'next/font/google';
 
 export const metadata: Metadata = {
   title: '투니즈 Toonyz',
@@ -35,5 +46,78 @@ export const metadata: Metadata = {
   }
 }
 
-import RootLayoutClient from '@/app/layout.client'
-export default RootLayoutClient
+
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+const notoSans = Noto_Sans({
+  subsets: ['latin', 'latin-ext', 'cyrillic', 'cyrillic-ext', 'greek', 'greek-ext', 'devanagari'],
+  weight: '400'
+})
+const notoSansKR = Noto_Sans_KR({
+  subsets: ['latin'],
+  weight: '400'
+})
+const notoSansArabic = Noto_Sans_Arabic({
+  subsets: ['arabic'],
+  weight: '400'
+})
+const notoSansThai = Noto_Sans_Thai({
+  subsets: ['thai'],
+  weight: '400'
+})
+const notoSansJP = Noto_Sans_JP({
+  subsets: ['latin'],
+  weight: '400'
+})
+const notoSansTC = Noto_Sans_TC({
+  subsets: ['latin'],
+  weight: '400'
+})
+const notoSansSC = Noto_Sans_SC({
+  subsets: ['latin'],
+  weight: '400'
+})
+
+
+
+export default function RootLayout({ children }: RootLayoutProps) {
+    return (
+      <html>
+        <body>
+          <LanguageProvider>
+            <ThemeProvider>
+              <AuthProvider>
+                <UserProvider>
+                  <DeviceProvider>
+                    <SearchProvider>
+                      <div className={`${notoSans.className} ${notoSansKR.className} ${notoSansArabic.className} 
+                    ${notoSansThai.className} ${notoSansJP.className} ${notoSansTC.className} ${notoSansSC.className}`}>
+                        <Suspense>
+                          <Header />
+                        </Suspense>
+                        <Margin>
+                          {children}
+                        </Margin>
+                        {/* 
+                    <div className={`children min-h-screen`}>  
+                     // Header bottom margin :: pt-28 md:pt-24 mb-4
+                      {children}
+                    </div> 
+                   */}
+                      </div>
+                    </SearchProvider>
+                  </DeviceProvider>
+                </UserProvider>
+              </AuthProvider>
+            </ThemeProvider>
+          </LanguageProvider>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.js" async />
+          <script src="https://kit.fontawesome.com/ca5078bbee.js" crossOrigin="anonymous" async></script>
+          <script src="https://cdn.iamport.kr/v1/iamport.js" async></script>
+        </body>
+      </html >
+    );
+  }
+  

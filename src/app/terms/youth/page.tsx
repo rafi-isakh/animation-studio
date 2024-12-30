ab78d644680ff9c138a9176b273013ac9cbb99c4
server <server@stelland.io>
2024-12-30 18:47:13 +0900

    Add english terms

--- src/app/terms/youth/page.tsx
+++ src/app/terms/youth/page.tsx
@@ -1,6 +1,9 @@
+"use client"
+import { useLanguage } from '@/contexts/LanguageContext';
 import { replaceSmartQuotes } from '@/utils/font';
-import {terms_youth} from '@/utils/terms';
+import {terms_youth, terms_youth_english} from '@/utils/terms';
 
 export default function Terms() {
-    return <div className='max-w-screen-md mx-auto p-4 whitespace-pre-wrap'>{replaceSmartQuotes(terms_youth)}</div>
+    const {language} = useLanguage();   
+    return <div className='max-w-screen-md mx-auto p-4 whitespace-pre-wrap'>{replaceSmartQuotes(language === 'ko' ? terms_youth : terms_youth_english)}</div>
 }

b7d492aa7affffdfdbb9093b721232f5d3d1e43b
Dami <dami@stelland.io>
2025-01-02 11:57:47 +0900

    fixed font size to be text-base.

--- src/components/StarsTransactionComponent.tsx
+++ src/components/StarsTransactionComponent.tsx
@@ -19,16 +19,16 @@ const StarsTransactionComponent = () => {
                 {/* 사용내역 ( 총 건수 / 사용 건수 / 잔여 건수 ) */}
                 <div className="flex flex-col w-full gap-2 bg-gray-100 rounded-xl px-5 py-5 mt-5">
                     {/* top padding 5 */}
-                    {language === 'ko' && <p className="text-sm text-gray-500 text-left">현재 보유중인 투니즈 별 <span className="font-bold text-[#DE2B74]">1000</span> 개 </p>}
-                    {language === 'ko' && <div className="flex flex-row items-center gap-2"> <CornerDownRight size={10} />  <p className="text-sm text-gray-500 text-left">이벤트 투니즈 별 <span className="font-bold text-[#DE2B74]">1000</span> 개 </p> </div>}
-                    {language === 'en' && <p className="text-sm text-gray-500 text-left ">You have <span className="font-bold text-[#DE2B74]">1000</span> stars </p>}
-                    {language === 'en' && <div className="flex flex-row items-center gap-2"> <CornerDownRight size={10} />  <p className="text-sm text-gray-500 text-left">Event <span className="font-bold text-[#DE2B74]">1000</span> stars </p> </div>}
+                    {language === 'ko' && <p className="text-base text-gray-500 text-left">현재 보유중인 투니즈 별 <span className="font-bold text-[#DE2B74]">1000</span> 개 </p>}
+                    {language === 'ko' && <div className="flex flex-row items-center gap-2"> <CornerDownRight size={10} />  <p className="text-base text-gray-500 text-left">이벤트 투니즈 별 <span className="font-bold text-[#DE2B74]">1000</span> 개 </p> </div>}
+                    {language === 'en' && <p className="text-base text-gray-500 text-left ">You have <span className="font-bold text-[#DE2B74]">1000</span> stars </p>}
+                    {language === 'en' && <div className="flex flex-row items-center gap-2"> <CornerDownRight size={10} />  <p className="text-base text-gray-500 text-left">Event <span className="font-bold text-[#DE2B74]">1000</span> stars </p> </div>}
                 </div>
 
                 <div className="flex flex-col w-full">
-                    <ul className="flex flex-col w-full gap-2 p-5 text-sm text-gray-500">
+                    <ul className="flex flex-col w-full gap-2 p-5 text-base text-gray-500">
                         <li className="flex flex-col w-full gap-2 pb-5 border-b border-gray-200 ">
-                            <div className="flex flex-row items-center gap-1">
+                            <div className="flex flex-row items-center">
                                 <Plus className="text-gray-500 w-4 h-4" />
                                 {/* 
                                     font size:

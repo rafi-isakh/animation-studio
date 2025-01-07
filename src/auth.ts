1b9c5400904aae0c737dac7e95ada788700f29fc
server <server@stelland.io>
2025-01-07 19:02:02 +0900

    Fix apple provider

--- src/auth.ts
+++ src/auth.ts
@@ -69,38 +69,31 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
     Kakao,
     Apple({
-      clientId: process.env.AUTH_APPLE_ID!,
-      clientSecret: process.env.AUTH_APPLE_SECRET!,
-      wellKnown: "https://appleid.apple.com/.well-known/openid-configuration",
+      clientId: process.env.AUTH_APPLE_ID,
+      clientSecret: ""+process.env.AUTH_APPLE_SECRET,
       checks: ["pkce"],
       token: {
         url: `https://appleid.apple.com/auth/token`,
       },
-      authorization: {
-        url: 'https://appleid.apple.com/auth/authorize',
-        params: {
-          scope: '',
-          response_type: 'code',
-          response_mode: 'query',
-          state: crypto.randomUUID()
-        },
-      },
       client: {
         token_endpoint_auth_method: "client_secret_post",
       },
-    })
+      authorization: {
+        params: {
+          response_mode: "form_post",
+          response_type: "code",//do not set to "code id_token" as it will not work
+          scope: "name email"
+        },},
+        profile(profile) {
+          return {
+            id: profile.sub,
+            name: "Person Doe",//profile.name.givenName + " " + profile.name.familyName, but apple does not return name...
+            email: profile.email,
+            image: "",
+          }
+        }
+    }),
   ],
-  cookies: {
-    pkceCodeVerifier: {
-      name: "next-auth.pkce.code_verifier",
-      options: {
-        httpOnly: true,
-        sameSite: "none",
-        path: "/",
-        secure: true,
-      },
-    },
-  },
   callbacks: {
     async jwt({ token, account, profile }) {
       // Initial sign in

26f47d554e6789797b802280442840d299d4cffc
server <server@stelland.io>
2025-01-13 10:24:14 +0900

    Don't log private key

--- src/auth.ts
+++ src/auth.ts
@@ -13,7 +13,6 @@ import { cookies } from "next/headers";
 
 const getAppleToken = async () => {
   const key = `-----BEGIN PRIVATE KEY-----\n${process.env.AUTH_APPLE_SECRET}\n-----END PRIVATE KEY-----\n`;
-  console.log("key", key)
   const appleToken = await new SignJWT({})
     .setAudience("https://appleid.apple.com")
     .setIssuer(process.env.AUTH_APPLE_TEAM_ID!)

3f8f37ef2e725120ce31c2c7b64370d7eb7755f5
Dami <dami@stelland.io>
2025-02-28 12:41:03 +0900

    working on share button

--- src/components/FloatingMenuComponent.tsx
+++ src/components/FloatingMenuComponent.tsx
@@ -429,3 +429,20 @@ const FloatingMenu: React.FC<{
 }
 
 export { FloatingMenu }
+
+
+function ShareForm({ className }: React.ComponentProps<"form">) {
+    return (
+      <form className={cn("grid items-start gap-4", className)}>
+        <div className="grid gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input type="email" id="email" defaultValue="shadcn@example.com" />
+        </div>
+        <div className="grid gap-2">
+          <Label htmlFor="username">Username</Label>
+          <Input id="username" defaultValue="@shadcn" />
+        </div>
+        <Button type="submit">Save changes</Button>
+      </form>
+    )
+  }

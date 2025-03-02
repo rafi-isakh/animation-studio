5423a951b39cda279b88f1e26b1f5d4d7fb8f03f
dami-stelland <dami@stelland.io>
2025-03-02 12:53:59 +0900

    fixed button css styling.

--- src/components/UI/BlobButton.tsx
+++ src/components/UI/BlobButton.tsx
@@ -5,11 +5,14 @@ const BlobButton: React.FC<{ text: React.ReactNode }> = ({ text }) => {
   return (
     <button
     // ${styles.button} 
-    className={` ${styles.button}  flex-shrink-1 w-full relative inline-flex items-center justify-center md:text-base text-md font-medium text-white transition-all duration-200 bg-transparent rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900`}
+    className={` ${styles.button} `}
+    //  flex-shrink-1 w-full relative inline-flex items-center justify-center md:text-base font-medium text-white transition-all duration-200 bg-transparent rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900
     >
         <span className={`${styles.text} backdrop-blur-sm`}>
           {text}
          </span>
+         <span className={styles.blob}></span>
+         <span className={styles.blob}></span>
          <span className={styles.blob}></span>
          <span className={styles.blob}></span>
     </button>

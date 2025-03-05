25ded76f250d813bfc0eabf997f0c69d1ca606c6
Dami <dami@stelland.io>
2025-03-05 11:37:29 +0900

    implement switch theme

--- src/components/UI/ViewerSettingDialog.tsx
+++ src/components/UI/ViewerSettingDialog.tsx
@@ -6,11 +6,15 @@ import { phrase } from "@/utils/phrases";
 import Link from "next/link";
 import { useTheme } from '@/contexts/providers'
 import { useReader } from '@/contexts/ReaderContext';
+import { Slider } from '@/components/shadcnUI/Slider';
+import { Switch } from '@/components/shadcnUI/Switch';
+import { Label } from '@/components/shadcnUI/Label';
+import { Moon, Sun } from 'lucide-react';
 
 export const ViewerSettingDialog = ({ showIsViewerModal, setShowIsViewerModal }: { showIsViewerModal: boolean, setShowIsViewerModal: (showIsViewerModal: boolean) => void }) => {
     const { dictionary, language } = useLanguage();
     const { theme, toggleTheme } = useTheme();
-    const { 
+    const {
         fontSize = 16,
         setFontSize,
         fontFamily = 'default',
@@ -19,7 +23,7 @@ export const ViewerSettingDialog = ({ showIsViewerModal, setShowIsViewerModal }:
         setLineHeight,
         scrollType = 'vertical',
         setScrollType,
-        setPage 
+        setPage
     } = useReader();
 
     return (
@@ -30,11 +34,13 @@ export const ViewerSettingDialog = ({ showIsViewerModal, setShowIsViewerModal }:
                 showCloseButton={true}
             >
                 <div className='flex flex-col space-y-4 text-black dark:text-white'>
-                    <p className='flex justify-between'>
-                        {/* View Settings */}
-                        {phrase(dictionary, "viewSettings", language)}
-                    </p>
-                    <hr className='my-2 border-gray-200' />
+                    <DialogHeader>
+                        <DialogTitle className='text-xl'>
+                            {/* View Settings */}
+                            {phrase(dictionary, "viewSettings", language)}
+                        </DialogTitle>
+                        <hr className='my-2 border-gray-200' />
+                    </DialogHeader>
 
                     <div className='text-sm justify-between md:flex hidden'>
                         {/* 넘김 방식 */}
@@ -53,20 +59,19 @@ export const ViewerSettingDialog = ({ showIsViewerModal, setShowIsViewerModal }:
                     <div className='text-sm flex justify-between'>
                         {/* 테마  */}
                         {phrase(dictionary, "theme", language)}
-                        <div className='flex flex-row gap-2'>
-                            <Link
-                                href=''
-                                onClick={() => toggleTheme('light')}
-                                className='text-[10px] bg-white text-black rounded-full border border-gray-400 px-2 py-1 self-center text-center'>
-                                Aa
-                            </Link>
-                            <Link
-                                href=''
-                                onClick={() => toggleTheme('dark')}
-                                className='text-[10px] bg-black text-white rounded-full border border-gray-400 px-2 py-1 self-center text-center'>
-                                Aa
-                            </Link>
+                        <div className="flex flex-row items-center justify-between gap-x-3">
+                            <div className="flex items-center gap-1">
+                                {theme === 'dark' ? <Moon className="h-4 w-4" /> : <Sun className="h-4 w-4" />}
+                                <Label htmlFor="dark-mode">Dark Mode</Label>
+                            </div>
+                            <Switch
+                                id="dark-mode"
+                                checked={theme === 'dark'}
+                                onCheckedChange={() => toggleTheme(theme === 'dark' ? 'light' : 'dark')}
+                                className="data-[state=checked]:bg-pink-800 data-[state=unchecked]:bg-gray-200"
+                            />
                         </div>
+
                     </div>
 
                     <div className='text-sm flex justify-between'>

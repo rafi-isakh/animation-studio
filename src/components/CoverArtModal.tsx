d3b20dedbf194a9236a9be5fdcd5db0b044d97ff
Dami <dami@stelland.io>
2025-03-14 16:28:23 +0900

    continue to fix upload cover art dialog for mobile

--- src/components/CoverArtModal.tsx
+++ src/components/CoverArtModal.tsx
@@ -10,6 +10,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { SetStateAction, Dispatch, useState } from 'react';
 import CoverArtPreview from './CoverArtPreview';
+import { ScrollArea } from '@/components/shadcnUI/ScrollArea';
 
 const CoverArtModal = ({
     showCoverArtModal,
@@ -47,100 +48,103 @@ const CoverArtModal = ({
 
     return (
         <Dialog open={showCoverArtModal} onOpenChange={setShowCoverArtModal}>
-            <DialogContent className='bg-white dark:bg-black flex flex-col justify-center items-center w-full h-screen'>
-                <DialogHeader className='flex flex-row justify-between items-center my-2 w-full'>
-                    <DialogTitle className='text-lg font-bold text-black dark:text-black'>
-                        {/* Cover Art Register */}
-                        {phrase(dictionary, "coverArtRegister", language)}
-                    </DialogTitle>
-                    <X size={24} className='text-black hover:text-[#DB2777] cursor-pointer self-end' onClick={handleClose} />
-                </DialogHeader>
+            <DialogContent className='bg-white dark:bg-black flex flex-col justify-center items-center w-full md:h-auto h-screen'>
+                <ScrollArea className='w-full h-full'>
+                    <DialogHeader className='flex flex-row justify-between items-center my-2 w-full'>
+                        <DialogTitle className='text-lg font-bold text-black dark:text-white'>
+                            {/* Cover Art Register */}
+                            {phrase(dictionary, "coverArtRegister", language)}
+                        </DialogTitle>
+                    </DialogHeader>
 
-                <div className='flex md:flex-row flex-col md:space-x-4 justify-center'>
-                    <div className='flex flex-col space-y-4'>
-                        <CoverArtPreview coverArt={coverArt} handleCoverArtUploadModal={handleCoverArtUploadModal} />
-                        <Button
-                            variant='outline'
-                            onClick={handleUploadFile}
-                            className="border-0 w-[200px] bg-[#DB2777] text-white"
-                        >
-                            {/* Upload Cover Art */}
-                            {phrase(dictionary, "upload", language)}
-                        </Button>
-                    </div>
-                    <div className='flex flex-col md:py-0 py-2 md:space-y-4 space-y-2'>
-                        <p className={`text-sm text-black font-bold ${language == 'ko' ? 'break-keep' : ''}`}>
-                            {/* Please upload the cover art for your webnovel. */}
-                            {phrase(dictionary, "pleaseUploadTheCoverArt", language)}
-                        </p>
+                    <div className='flex md:flex-row flex-col md:space-x-4 justify-center'>
+                        <div className='flex flex-col space-y-4'>
+                            <CoverArtPreview coverArt={coverArt} handleCoverArtUploadModal={handleCoverArtUploadModal} />
+                            <Button
+                                variant='outline'
+                                onClick={handleUploadFile}
+                                className="border-0 w-[200px] bg-[#DB2777] text-white"
+                            >
+                                {/* Upload Cover Art */}
+                                {phrase(dictionary, "upload", language)}
+                            </Button>
+                        </div>
+                        <div className='flex flex-col md:py-0 py-2 md:space-y-4 space-y-2'>
+                            <p className={`text-sm text-black font-bold ${language == 'ko' ? 'break-keep' : ''}`}>
+                                {/* Please upload the cover art for your webnovel. */}
+                                {phrase(dictionary, "pleaseUploadTheCoverArt", language)}
+                            </p>
 
-                        <p className={`text-sm text-gray-500 font-bold ${language == 'ko' ? 'break-keep' : ''}`}>
-                            {/* The cover art must be in a 5:3 aspect ratio. */}
-                            {phrase(dictionary, "coverArtMustBeInA53AspectRatio", language)}
-                        </p>
-                        <p className={`text-sm text-gray-500 ${language == 'ko' ? 'break-keep' : ''}`}>
-                            {/*
+                            <p className={`text-sm text-gray-500 font-bold ${language == 'ko' ? 'break-keep' : ''}`}>
+                                {/* The cover art must be in a 5:3 aspect ratio. */}
+                                {phrase(dictionary, "coverArtMustBeInA53AspectRatio", language)}
+                            </p>
+                            <p className={`text-sm text-gray-500 ${language == 'ko' ? 'break-keep' : ''}`}>
+                                {/*
                         It ideally sized at 1500 x 900 px, but can be smaller than 1000 x 600 px.
                         It must be in PNG, GIF, or JPG format, smaller than 2MB. */}
-                            {phrase(dictionary, "coverArtSize", language)}
-                        </p>
-
-                        <div className='flex flex-row gap-1 items-center'>
-                            <Info size={12} className='self-center text-[#DB2777]' />
-                            <p className='text-sm text-[#DB2777]'>
-                                {/* Mature Content and Cover Art Violence rules */}
-                                {phrase(dictionary, "matureContentAndCoverArtViolenceRules", language)}
+                                {phrase(dictionary, "coverArtSize", language)}
                             </p>
-                        </div>
 
-                        <p className={`text-sm text-gray-500 ${language == 'ko' ? 'break-keep' : ''}`}>
-                            {/* Book cover must be safe for all ages.
+                            <div className='flex flex-row gap-1 items-center'>
+                                <Info size={12} className='self-center text-[#DB2777]' />
+                                <p className='text-sm text-[#DB2777]'>
+                                    {/* Mature Content and Cover Art Violence rules */}
+                                    {phrase(dictionary, "matureContentAndCoverArtViolenceRules", language)}
+                                </p>
+                            </div>
+
+                            <p className={`text-sm text-gray-500 ${language == 'ko' ? 'break-keep' : ''}`}>
+                                {/* Book cover must be safe for all ages.
                             Graphic violence, sexual nudity, and hateful content are strictly prohibited. */}
-                            {phrase(dictionary, "bookCoverMustBeSafeForAllAges", language)}
-                        </p>
-                        <div className='grid grid-cols-4 gap-2 overflow-y-auto h-60 p-1'>
-                            {coverArtOptions.map((option) => (
-                                <label key={option.id}>
-                                    <input
-                                        type="radio"
-                                        name="Selecting_CoverArt"
-                                        value={option.value}
-                                    />
-                                    <Image
-                                        onClick={() => {
-                                            fetch(option.src)
-                                                .then(res => res.blob())
-                                                .then(blob => {
-                                                    const file = new File([blob], '/coverArt.jpg', { type: 'image/jpeg' });
-                                                    setCoverArtFile(file);
-                                                });
-                                        }}
-                                        src={option.src}
-                                        alt={`Option ${option.id}`}
-                                        width={200}
-                                        height={300}
-                                    />
-                                </label>
-                            ))}
+                                {phrase(dictionary, "bookCoverMustBeSafeForAllAges", language)}
+                            </p>
+                            <div className='grid grid-cols-4 gap-2 overflow-y-auto h-60 p-1'>
+                                {coverArtOptions.map((option) => (
+                                    <label key={option.id}>
+                                        <input
+                                            type="radio"
+                                            name="Selecting_CoverArt"
+                                            value={option.value}
+                                        />
+                                        <Image
+                                            onClick={() => {
+                                                fetch(option.src)
+                                                    .then(res => res.blob())
+                                                    .then(blob => {
+                                                        const file = new File([blob], '/coverArt.jpg', { type: 'image/jpeg' });
+                                                        setCoverArtFile(file);
+                                                    });
+                                            }}
+                                            src={option.src}
+                                            alt={`Option ${option.id}`}
+                                            width={200}
+                                            height={300}
+                                        />
+                                    </label>
+                                ))}
+                            </div>
                         </div>
-
-                        <DialogFooter className='flex flex-row space-x-4 justify-center items-center'>
-                            <Button variant='outline' className='mt-10 w-32 text-blue-500 dark:text-blue-500' onClick={handleConfirm}>
-                                {phrase(dictionary, "confirm", language)}
-                            </Button>
-                            <Button
-                                onClick={handleClose}
-                                color='gray'
-                                variant='outline'
-                                className='mt-10 w-32 text-[#DB2777] dark:text-[#DB2777] hover:bg-[#DB2777] hover:text-white dark:hover:bg-[#DB2777] dark:hover:text-white'>
-                                {phrase(dictionary, "cancel", language)}
-                            </Button>
-                        </DialogFooter>
                     </div>
-                </div>
+                    <DialogFooter className='flex flex-row gap-2 my-5 md:py-0 pb-10 justify-center items-center'>
+                        <Button
+                            onClick={handleConfirm}
+                            variant='outline'
+                            className='w-32 text-blue-500 dark:text-blue-500'
+                        >
+                            {phrase(dictionary, "confirm", language)}
+                        </Button>
+                        <Button
+                            onClick={handleClose}
+                            variant='outline'
+                            className='w-32 text-[#DB2777] dark:text-[#DB2777] hover:bg-[#DB2777] hover:text-white dark:hover:bg-[#DB2777] dark:hover:text-white'
+                        >
+                            {phrase(dictionary, "cancel", language)}
+                        </Button>
+                    </DialogFooter>
+                </ScrollArea>
             </DialogContent>
         </Dialog>
-
     )
 }
 

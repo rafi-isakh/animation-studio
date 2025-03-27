1810a41ccb1f6ac6c2f936ec34a7292941563009
server <server@stelland.io>
2025-03-27 11:09:02 +0900

    Focus on search input

--- src/components/SearchComponent.tsx
+++ src/components/SearchComponent.tsx
@@ -120,6 +120,12 @@ export default function SearchComponent({
         setOpen(open);
     };
 
+    useEffect(() => {
+        if (open) {
+            inputRef.current?.focus();
+        }
+    }, [open])
+
     const renderSearchInput = () => (
         <div className="relative max-w-screen-xl flex-1 h-12 mx-auto">
             <Search
@@ -131,6 +137,7 @@ export default function SearchComponent({
                 type="text"
                 id="search-navbar"
                 value={query}
+                autoFocus
                 onChange={handleChange}
                 placeholder={query ? query : phrase(dictionary, "searchPlaceholder", language)}
                 className="w-full h-full p-2 pl-10 text-sm border-0 

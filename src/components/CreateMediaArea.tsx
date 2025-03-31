4d3c3556aec01a16615af26adf913251873b304d
Dami <dami@stelland.io>
2025-03-31 11:05:40 +0900

    fix scrollArea z-index

--- src/components/CreateMediaArea.tsx
+++ src/components/CreateMediaArea.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { Skeleton } from "./shadcnUI/Skeleton";
-import { RefreshCw, Video, X, RefreshCcw } from "lucide-react";
+import { RefreshCw, Video, X, RefreshCcw, EllipsisVertical, History } from "lucide-react";
 import { Sparkles, Loader2 } from "lucide-react";
 import { Button } from "./shadcnUI/Button";
 import GeneratedPicture from "./GeneratedPicture";
@@ -20,6 +20,11 @@ import CreateMediaDefaultContents from "@/components/UI/CreateMediaDefaultConten
 import { useLanguage } from "@/contexts/LanguageContext";
 import { phrase } from "@/utils/phrases";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/shadcnUI/Tooltip"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/shadcnUI/Popover"
 import { make_video_price, generate_pictures_price, generate_trailer_price } from "@/utils/stars";
 import { MdStars } from "react-icons/md";
 
@@ -88,17 +93,19 @@ export default function CreateMediaArea({
                            dark:bg-[#2C2C2C] bg-[#F5F5F5]
                             rounded-lg no-scrollbar flex flex-col gap-0 transition-opacity duration-300
                             ${openDialog ? 'block z-[100]' : 'hidden'}`}
-                            //  bg-gradient-to-r dark:from-gray-900/10 dark:to-blue-900/10 from-white/50 to-blue-100/50 backdrop-blur-md
+                //  bg-gradient-to-r dark:from-gray-900/10 dark:to-blue-900/10 from-white/50 to-blue-100/50 backdrop-blur-md
                 onClick={(e) => e.stopPropagation()}
             >
                 <div className='drag-handle flex-shrink-0'>
                     <div className="flex flex-col p-4 justify-center">
                         <div className="flex items-center justify-between border-b pb-2">
-                            <div className="flex items-center">               
+                            <div className="flex items-center">
                                 <div className="flex items-center justify-center gap-2">
-                                    <h1 className="text-xl font-medium uppercase">Toonyz Post</h1>
-                                    <h1 className="text-lg md:text-xl text-gray-700 dark:text-gray-300 flex items-center gap-2">
-                                        <MdStars className="text-lg md:text-xl text-[#D92979]" /> {phrase(dictionary, "star", language)} {stars}{phrase(dictionary, "count", language)}
+                                    <h1 className="md:text-xl text-md font-medium uppercase">Toonyz Post</h1>
+                                    <h1 className="md:text-xl text-md text-gray-700 dark:text-gray-300 flex items-center gap-1">
+                                        <MdStars className="text-lg md:text-xl text-[#D92979]" />
+                                        <span className="text-gray-700 dark:text-gray-300">{phrase(dictionary, "star", language)}</span>
+                                        <span className="text-gray-700 dark:text-gray-300">{stars}{phrase(dictionary, "count", language)}</span>
                                     </h1>
                                 </div>
                             </div>
@@ -106,24 +113,50 @@ export default function CreateMediaArea({
                                 <TooltipProvider delayDuration={0}>
                                     <Tooltip>
                                         <TooltipTrigger asChild>
-                                            <Button variant="ghost" size="icon" className="rounded-full h-9 w-9"
-                                                onClick={(e) => {
-                                                    e.preventDefault();
-                                                    e.stopPropagation()
-                                                    setIsLoading(false);
-                                                    setLoadingVideoGeneration(false);
-                                                    setPictures([]);
-                                                    setSelection('');
-                                                    setNarrations([]);
-                                                }}>
-                                                <RefreshCcw className="h-5 w-5" />
-                                            </Button>
+                                            <Popover>
+                                                <PopoverTrigger asChild>
+                                                    <Button variant="ghost" size="icon" className="rounded-full h-9 w-9">
+                                                        <EllipsisVertical className="h-5 w-5" />
+                                                    </Button>
+                                                </PopoverTrigger>
+                                                <PopoverContent className="relative w-fit mx-auto p-1">
+                                                    <ul className="relative flex flex-col justify-start items-start gap-1 text-sm">
+                                                        <Button variant="ghost" className="w-full flex flex-row justify-start items-center"
+                                                            onClick={(e) => {
+                                                                e.preventDefault();
+                                                                e.stopPropagation()
+                                                                setIsLoading(false);
+                                                                setLoadingVideoGeneration(false);
+                                                                setPictures([]);
+                                                                setSelection('');
+                                                                setNarrations([]);
+                                                            }}>
+                                                            <RefreshCcw className="h-5 w-5" />
+                                                             <span className="text-gray-700 dark:text-gray-300">Reset</span>
+                                                        </Button>
+                                                        <Button variant="ghost" className="w-full flex flex-row justify-start items-center"
+                                                            onClick={(e) => {
+                                                                e.preventDefault();
+                                                                e.stopPropagation()
+                                                                setIsLoading(false);
+                                                                setLoadingVideoGeneration(false);
+                                                                setPictures([]);
+                                                                setSelection('');
+                                                                setNarrations([]);
+                                                            }}>
+                                                            <History className="h-5 w-5" />
+                                                             <span className="text-gray-700 dark:text-gray-300">History</span>
+                                                        </Button>
+                                                    </ul>
+                                                </PopoverContent>
+                                            </Popover>
                                         </TooltipTrigger>
                                         <TooltipContent>
-                                            Reset
+                                            More options
                                         </TooltipContent>
                                     </Tooltip>
                                 </TooltipProvider>
+
                                 <Button variant="ghost" size="icon" className="rounded-full h-9 w-9"
                                     onClick={(e) => {
                                         e.preventDefault();
@@ -137,7 +170,7 @@ export default function CreateMediaArea({
                         </div>
                     </div>
                 </div>
-                <ScrollArea className='drag-handle flex-1 overflow-auto no-scrollbar'>
+                <ScrollArea className='drag-handle flex-1 overflow-auto no-scrollbar z-[100]'>
                     <div className='relative w-full p-1'>
                         {isLoading ? (
                             <div className="flex flex-col gap-4 max-[360px]:w-full md:max-w-[425px] w-full">
@@ -208,7 +241,7 @@ export default function CreateMediaArea({
                                         </div>
 
                                         <div className="h-20 flex items-center justify-start  gap-2 overflow-x-auto w-full no-scrollbar scrollbar-hide" style={{ WebkitOverflowScrolling: 'touch' }}>
-                                          
+
                                             <Button
                                                 variant="outline"
                                                 className="p-4 md:p-3 min-h-[48px] rounded-full bg-gray-300 dark:bg-[#1a1b1f] text-black dark:text-white dark:border-[#2a2b2f] hover:text-white hover:bg-[#2a2b2f] flex gap-2 shrink-0 shadow-none"
--- src/components/UI/CreateMediaDefaultContents.tsx
+++ src/components/UI/CreateMediaDefaultContents.tsx
@@ -58,7 +58,8 @@ export default function CreateMediaDefaultContents({ stars, source, chapterIds }
 
 
     return (
-        <div className="min-h-screen bg-gradient-to-r dark:from-black dark:to-transparent from-transparent to-blue-100/50 backdrop-blur-md  p-6 md:p-8">
+        <div className="min-h-screen  p-6 md:p-8">
+            {/* bg-gradient-to-r dark:from-black dark:to-transparent from-transparent to-blue-100/50 backdrop-blur-md  */}
             {/* Header Section */}
             <div className="max-w-4xl mx-auto mb-12">
                 <h1 className="text-2xl md:text-2xl font-bold mb-4 uppercase text-gray-700 dark:text-gray-300 break-words">

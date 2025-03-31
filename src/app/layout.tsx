1cc7108110818b70bfb3e9e21d0f58db3266ee81
server <server@stelland.io>
2025-03-31 19:05:36 +0900

    Add ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -118,6 +118,8 @@ export default async function RootLayout({ children }: RootLayoutProps) {
           </LanguageProvider>
         </ToastProvider>
         <script src="https://kit.fontawesome.com/ca5078bbee.js" crossOrigin="anonymous" async></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6123598702567464"
+                crossOrigin="anonymous"></script>
       </body>
     </html >
   );

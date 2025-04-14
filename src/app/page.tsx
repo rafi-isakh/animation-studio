9aa3693f3682de7aabe6a9b9703717e51c203247
server <server@stelland.io>
2025-04-14 14:39:17 +0900

    get_library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,8 @@ async function getLibrary() {
         console.error("Failed to fetch library", response.status);
     }
     const data = await response.json();
-    return data.library;
+    console.log(data)
+    return data;
 }
 
 async function getToonyzPosts() {
@@ -51,9 +52,9 @@ async function getToonyzPosts() {
 
 export default async function Home({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
     let items = await getCarouselItems();
-    //let library = await getLibrary() || [];
-    let posts = await getToonyzPosts();
-    //library = library.filter((novel: Webnovel) => !temporarilyUnpublished.includes(novel.id));
+    let library = await getLibrary() || [];
+    // let posts = await getToonyzPosts();
+    library = library.filter((novel: Webnovel) => !temporarilyUnpublished.includes(novel.id));
     const carouselFilter = [22, 24, 19]
     items = items.filter((item: any) => !carouselFilter.includes(item.webnovel_id));
 

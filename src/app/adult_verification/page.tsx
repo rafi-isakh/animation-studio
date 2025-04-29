4a39ad3e4430ae9883602ce3d666d7e229b1c239
server <server@stelland.io>
2025-04-29 09:58:12 +0900

    Adult verification

--- src/app/adult_verification/page.tsx
+++ src/app/adult_verification/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { v4 as uuidv4 } from 'uuid';
 import { useUser } from "@/contexts/UserContext";
 
-export default function AdultVerificationPage({params: {webnovel_id}}: {params: {webnovel_id: string}}) {
+export default function AdultVerificationPage() {
     const router = useRouter();
+    const searchParams = useSearchParams();
     const { setIsAdult } = useUser();
+    const webnovel_id = searchParams.get('webnovel_id') as string;
 
     useEffect(() => {
         if (!window.IMP) return;
@@ -17,43 +19,44 @@ export default function AdultVerificationPage({params: {webnovel_id}}: {params: 
         // @ts-ignore
         IMP.certification(
             {
-              // param
-              channelKey: "channel-key-40f1455b-2c0b-4379-a134-c91a2e76e340",
-              merchant_uid: uuidv4(), // 주문 번호
-              m_redirect_url: "{리디렉션 될 URL}", // 모바일환경에서 popup:false(기본값) 인 경우 필수, 예: https://www.myservice.com/payments/complete/mobile
-              popup: false, // PC환경에서는 popup 파라미터가 무시되고 항상 true 로 적용됨
+                // param
+                channelKey: "channel-key-40f1455b-2c0b-4379-a134-c91a2e76e340",
+                merchant_uid: uuidv4(), // 주문 번호
+                m_redirect_url: `${process.env.NEXT_PUBLIC_HOST}/adult_verification/redirect?webnovel_id=${webnovel_id}`, // 모바일환경에서 popup:false(기본값) 인 경우 필수, 예: https://www.myservice.com/payments/complete/mobile
+                popup: false, // PC환경에서는 popup 파라미터가 무시되고 항상 true 로 적용됨
             },
             function (rsp: any) {
-              // callback
-              if (rsp.success) {
-                // 인증 성공 시 로직
-                alert("인증 성공");
-                fetch("/api/verify_as_adult", {
-                    method: "POST",
-                    body: JSON.stringify({
-                        imp_uid: rsp.imp_uid,
-                    }),
-                }).then(response => {
-                    if (response.ok) {
-                        alert("업데이트 성공");
-                        setIsAdult(true);
-                        router.push(`/view_webnovels/${webnovel_id}`);
-                    } else {
-                        alert("업데이트 실패");
-                        router.push('/')
-                    }
-                });
-              } else {
-                // 인증 실패 시 로직
-                alert(`인증 실패. ${rsp.error_msg}`);
-                router.push('/')
-              }
+                // callback
+                if (rsp.success) {
+                    // 인증 성공 시 로직
+                    alert("인증 성공");
+                    fetch("/api/verify_as_adult", {
+                        method: "POST",
+                        body: JSON.stringify({
+                            imp_uid: rsp.imp_uid,
+                        }),
+                    }).then(response => {
+                        if (response.ok) {
+                            alert("업데이트 성공");
+                            setIsAdult(true);
+                            router.push(`/view_webnovels/${webnovel_id}`);
+                        } else {
+                            alert("업데이트 실패");
+                            router.push('/')
+                        }
+                    });
+                } else {
+                    // 인증 실패 시 로직
+                    alert(`인증 실패. ${rsp.error_msg}`);
+                    router.push('/')
+                }
             },
-          );
+        );
     }, []);
 
     return (
-        <div>
+        <div className="flex justify-center items-center h-screen">
+            <h1 className="text-2xl font-bold mb-4 text-gray-800 dark:text-white">성인 인증이 진행중입니다.</h1>
         </div>
     )
 }
--- src/app/api/verify_as_adult/route.ts
+++ src/app/api/verify_as_adult/route.ts
@@ -6,14 +6,43 @@ export async function POST(request: NextRequest) {
     if (!session) {
         return new NextResponse("Unauthorized", { status: 401 });
     }
-    const body = await request.json();
+    const { imp_uid } = await request.json();
+
+    const getToken = await fetch("https://api.iamport.kr/users/getToken", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            // REST API키
+            imp_key: process.env.PORTONE_API_KEY,
+            imp_secret: process.env.PORTONE_API_SECRET,
+        }),
+    });
+    if (!getToken.ok) {
+        return new NextResponse("Failed to get token", { status: getToken.status });
+    }
+    const {access_token} = await getToken.json();
+
+    const getCertifications = await fetch(`https://api.iamport.kr/certifications/${imp_uid}`, {
+        headers: { Authorization: access_token },
+    });
+    if (!getCertifications.ok) {
+        return new NextResponse("Failed to get certifications", { status: getCertifications.status });
+    }
+    const { birth } = await getCertifications.json();
+
+    const today = new Date();
+    const eighteenYearsAgo = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
+    if (new Date(birth) < eighteenYearsAgo) {
+        return new NextResponse(`Too young: ${birth}`, { status: 401 });
+    }
+
     const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND}/api/verify_as_adult`, {
         method: "POST",
         headers: {
             "Authorization": `Bearer ${session.accessToken}`,
             "Provider": session.provider,
         },
-        body: JSON.stringify(body),
+        body: JSON.stringify({ imp_uid }),
     });
     if (!response.ok) {
         const error = await response.text();

83991f913d8762b6408387c01817c91874637371
Dami <dami@stelland.io>
2025-04-29 16:15:38 +0900

    modified templateType === 'report'

--- src/components/UI/ReportButton.tsx
+++ src/components/UI/ReportButton.tsx
@@ -18,7 +18,7 @@ export default function ReportButton({ user, mode = "profile_page" }: { user: Us
         const message = `Reported user: ${user.nickname}\nUser ID: ${user.id}\n\nReport message: ${reportMessage}`;
         await fetch('/api/send_email', {
             method: 'POST',
-            body: JSON.stringify({ message: message, templateType: 'Report', subject: 'Report' })
+            body: JSON.stringify({ message: message, templateType: 'report', subject: 'Report' })
         });
         setShowReportModal(false);
         setShowReportSuccessModal(true);
--- src/utils/EmailTemplate.tsx
+++ src/utils/EmailTemplate.tsx
@@ -86,3 +86,6 @@ interface EmailTemplateProps {
       </html>
     `
   }
+
+
+ 

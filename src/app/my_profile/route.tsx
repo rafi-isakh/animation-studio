d25a5ef312125299df4f4319c23d5a0eeade9ea5
server <server@stelland.io>
2025-04-30 11:26:02 +0900

    Build error

--- src/app/my_profile/route.tsx
+++ src/app/my_profile/route.tsx
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { auth } from '@/auth';
-import { Webnovel } from "@/components/Types"e
+import { Webnovel } from "@/components/Types"
 import { signOut } from '@/auth';
 
 export async function GET(request: Request) {

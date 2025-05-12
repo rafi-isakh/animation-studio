75581a27ba125672ef384c4f919ea1c694a9453d
Dami <dami@stelland.io>
2025-05-12 14:11:10 +0900

    Update: Terms and Service modal

--- src/components/TermsOfServiceModal.tsx
+++ src/components/TermsOfServiceModal.tsx
@@ -7,8 +7,8 @@ import { useLanguage } from '@/contexts/LanguageContext';
 import { phrase } from '@/utils/phrases';
 import { replaceSmartQuotes } from '@/utils/font';
 import { WebnovelTerms, WebnovelTerms_en } from '@/utils/terms';
-import { HiOutlineExclamationCircle } from "react-icons/hi";
-import { MoveLeft } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { WarningModal } from './AddWebnovelComponent';
 
 interface TermsOfServiceModalProps {
     open: boolean;
@@ -25,13 +25,12 @@ const TermsOfServiceModal = ({
 }: TermsOfServiceModalProps) => {
     const [agreementOne, setAgreementOne] = useState(false);
     const [agreementTwo, setAgreementTwo] = useState(false);
-    const [isFieldValidationModalOpen, setIsFieldValidationModalOpen] = useState(false);
+    const [openWarningModal, setOpenWarningModal] = useState(false);
     const { dictionary, language } = useLanguage();
 
     const handleSubmit = async () => {
         if (!agreementOne || !agreementTwo) {
-            setIsFieldValidationModalOpen(true);
-            // alert(phrase(dictionary, "pleaseAgreeToTerms", language));
+            setOpenWarningModal(true);
             return;
         }
         await onSubmit();
@@ -39,19 +38,13 @@ const TermsOfServiceModal = ({
 
     return (
         <Dialog open={open} onOpenChange={onClose}>
-            <DialogContent className='bg-white dark:bg-black flex flex-col justify-center items-center w-full md:h-auto h-screen'>
-                <DialogHeader className='flex flex-row justify-start items-center my-2 w-full'>
-                    <Button
-                        variant='link'
-                        onClick={() => onClose()}
-                        className={`!no-underline justify-center items-center gap-1 text-gray-500 hover:text-gray-700 transition-colors flex md:hidden !m-0 !p-0`}>
-                        <MoveLeft size={20} className='dark:text-white text-gray-500' />
-                    </Button>
-                    <DialogTitle className='text-lg font-bold text-black dark:text-white text-start'>
-                        <p className='md:ml-0 ml-5'>{phrase(dictionary, "guideToRegisteringYourWork", language)}</p>
+            <DialogContent className="z-[2500] !gap-0 !p-0 overflow-hidden bg-white dark:bg-[#211F21] border-none shadow-none md:h-auto h-screen" showCloseButton={true}>
+                <DialogHeader className='flex w-full p-4'>
+                    <DialogTitle className='text-lg font-bold text-black dark:text-white'>
+                        <p className=''>{phrase(dictionary, "guideToRegisteringYourWork", language)}</p>
                     </DialogTitle>
                 </DialogHeader>
-                <div className='flex flex-col space-y-4 text-[12px]'>
+                <div className='flex flex-col space-y-4 text-xs p-4'>
                     {/* Terms content */}
                     <div className="flex flex-col">
                         <div className="max-h-[400px] overflow-y-auto p-4 bg-gray-50 rounded-lg text-sm">
@@ -100,41 +93,33 @@ const TermsOfServiceModal = ({
                             label={phrase(dictionary, 'agree_writing_terms_2', language)}
                         />
                     </div>
-                    <DialogFooter className="flex flex-row justify-center items-center gap-4">
-                        <Button
-                            variant='outline'
-                            onClick={handleSubmit}
-                            disabled={isSubmitting}
-                        >
-                            {isSubmitting ?
-                                <CircularProgress size="1rem" color='secondary' />
-                                : phrase(dictionary, "confirm", language)}
-                        </Button>
-                        <Button
-                            variant='outline'
-                            onClick={onClose}
-                        >
-                            {phrase(dictionary, "cancel", language)}
-                        </Button>
-                    </DialogFooter>
                 </div>
+                <DialogFooter className='flex flex-row !space-x-0 !p-0 !flex-grow-0 !flex-shrink-0 w-full self-end'>
+                    <Button
+                        onClick={handleSubmit}
+                        disabled={isSubmitting}
+                        className={cn("!rounded-none flex-1 w-full py-6 text-lg font-medium bg-[#DE2B74] hover:bg-[#DE2B74] text-white")}
+                    >
+                        {isSubmitting ?
+                            <CircularProgress size="1rem" color='secondary' />
+                            : phrase(dictionary, "confirm", language)}
+                    </Button>
+                    <Button
+                        onClick={onClose}
+                        className={cn("!rounded-none flex-1 w-full py-6 text-lg font-medium bg-[#b8c1d1] hover:bg-[#a9b2c2] text-white")}
+                    >
+                        {phrase(dictionary, "cancel", language)}
+                    </Button>
+                </DialogFooter>
             </DialogContent>
             {/* modal for input all info */}
-            <Dialog open={isFieldValidationModalOpen} onOpenChange={setIsFieldValidationModalOpen}>
-                <DialogContent className='bg-white dark:bg-black flex flex-col justify-center items-center'>
-                    <Typography className="text-center">
-                        <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
-                        <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                            {phrase(dictionary, "pleaseAgreeToTerms", language)}
-                        </h3>
-                        <div className="flex justify-center gap-4">
-                            <Button color='gray' variant='outline' onClick={() => setIsFieldValidationModalOpen(false)}>
-                                {phrase(dictionary, "ok", language)}
-                            </Button>
-                        </div>
-                    </Typography>
-                </DialogContent>
-            </Dialog>
+            <WarningModal
+                mode='agreeToTerms'
+                dictionary={dictionary}
+                language={language}
+                open={openWarningModal}
+                onOpenChange={setOpenWarningModal}
+            />
         </Dialog>
     );
 };

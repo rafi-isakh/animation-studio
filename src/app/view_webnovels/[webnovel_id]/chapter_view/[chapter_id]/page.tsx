6259ab2c899c28f186b0a1bcd532bdda7cc831ca
Dami <dami@stelland.io>
2025-05-13 15:30:28 +0900

    fix build error

--- src/app/view_webnovels/[webnovel_id]/chapter_view/[chapter_id]/page.tsx
+++ src/app/view_webnovels/[webnovel_id]/chapter_view/[chapter_id]/page.tsx
@@ -10,7 +10,6 @@ import WebnovelTranslateComponent from "@/components/WebnovelTranslateComponent"
 import { useLanguage } from "@/contexts/LanguageContext";
 import OtherTranslateComponent from "@/components/OtherTranslateComponent";
 import { Button } from "@/components/shadcnUI/Button";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from "@/components/shadcnUI/Dialog";
 import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarTrigger, MenubarShortcut } from "@/components/shadcnUI/Menubar";
 import { ChevronRight, ChevronLeft, Trash2, Heart, List, Type, Pencil } from 'lucide-react'
 import { usePathname, useRouter } from "next/navigation";
@@ -27,6 +26,7 @@ import { useWebnovels } from "@/contexts/WebnovelsContext";
 import ViewerSettingDialog from '@/components/UI/ViewerSettingDialog';
 import dynamic from 'next/dynamic';
 import { createEmailHash } from '@/utils/cryptography';
+import DeleteChapterDialog from "@/components/UI/DeleteChapterDialog";
 const LottieLoader = dynamic(() => import('@/components/LottieLoader'), {
     ssr: false,
 });
@@ -35,36 +35,7 @@ import CommentsComponent from "@/components/CommentsComponent";
 import ChapterPurchaseDialog from "@/components/UI/ChapterPurchaseDialog";
 import NotEnoughStarsDialog from "@/components/UI/NotEnoughStarsDialog";
 import { isPurchasedChapter } from "@/utils/webnovelUtils";
-import { cn } from "@/lib/utils";
 
-export const DeleteChapterDialog = ({ language, dictionary, showDeleteModal, setShowDeleteModal, deleteChapterId, handleChapterDelete }: { language: Language, dictionary: Dictionary, showDeleteModal: boolean, setShowDeleteModal: (showDeleteModal: boolean) => void, deleteChapterId: number | null, handleChapterDelete: (chapterId: number) => void }) => {
-    return (
-        <Dialog open={showDeleteModal} onOpenChange={() => setShowDeleteModal(false)}>
-            <DialogContent className='z-[2500] !gap-0 !p-0 overflow-hidden bg-white dark:bg-[#211F21] border-none shadow-none md:h-auto h-screen' showCloseButton={true}>
-                <DialogHeader className='p-4'>
-                    <DialogTitle>{phrase(dictionary, "deleteChapterConfirm", language)}</DialogTitle>
-                    <DialogDescription className='py-4'>
-                        <p>{phrase(dictionary, "deleteChapterConfirmDescription", language)}</p>
-                    </DialogDescription>
-                </DialogHeader>
-                <DialogFooter className='flex flex-row !space-x-0 !p-0 !flex-grow-0 !flex-shrink-0 w-full self-end'>
-                    <Button
-                        onClick={() => handleChapterDelete(deleteChapterId as number)}
-                        className={cn("!rounded-none flex-1 w-full py-6 text-lg font-medium bg-[#DE2B74] hover:bg-[#DE2B74] text-white")}
-                    >
-                        {phrase(dictionary, "yes", language)}
-                    </Button>
-                    <Button
-                        onClick={() => setShowDeleteModal(false)}
-                        className={cn("!rounded-none flex-1 w-full py-6 text-lg font-medium bg-[#b8c1d1] hover:bg-[#a9b2c2] text-white")}
-                    >
-                        {phrase(dictionary, "no", language)}
-                    </Button>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
-    )
-}
 
 function ChapterView({ params: { chapter_id, webnovel_id }, }: { params: { chapter_id: string, webnovel_id: string } }) {
     const [webnovel, setWebnovel] = useState<Webnovel>();

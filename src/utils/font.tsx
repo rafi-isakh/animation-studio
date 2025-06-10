8e051fb2f9d4940c7d895fa3c94aabf21485c6bc
server <server@stelland.io>
2025-06-10 10:49:53 +0900

    Postprocess text before displaying so newlines become two newlines, but not four

--- src/utils/font.tsx
+++ src/utils/font.tsx
@@ -1,5 +1,5 @@
 export function textPostProcess(text: string): string {
-    return text?.replaceAll(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"').replaceAll("  ", " ");
+    return text?.replaceAll(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"').replaceAll("  ", " ").replaceAll("\n", "\n\n").replaceAll("\n\n\n\n", "\n\n");
   }
 
 export function replaceSmartQuotes(text: string): string {

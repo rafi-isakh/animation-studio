ee316d83b6ebbb3499919208d0508cb6c74200e4
server <server@stelland.io>
2025-06-11 14:52:54 +0900

    Normalize newlines

--- src/utils/font.tsx
+++ src/utils/font.tsx
@@ -1,11 +1,11 @@
 export function textPostProcess(text: string): string {
-    return replaceSingleNewlines(text?.replaceAll(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"').replaceAll("  ", " "))
-  }
+  return normalizeNewlines(text?.replaceAll(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"').replaceAll("  ", " "))
+}
 
 export function replaceSmartQuotes(text: string): string {
-    return text?.replaceAll(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"');
-  }
+  return text?.replaceAll(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"');
+}
 
-  function replaceSingleNewlines(text: string): string {
-    return text.replace(/(?<!\n)\n(?!\n)/g, '\n\n');
-  }
+function normalizeNewlines(text: string): string {
+  return text.replace(/\n+/g, '\n\n');
+}

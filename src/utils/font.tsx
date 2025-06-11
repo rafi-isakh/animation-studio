13b9c478b17468bcada8693eb70be653306d4f73
server <server@stelland.io>
2025-06-11 16:08:52 +0900

    Normalize newlines only when they're not translated

--- src/utils/font.tsx
+++ src/utils/font.tsx
@@ -1,6 +1,8 @@
-export function textPostProcess(text: string, is_premium: boolean): string {
+export function textPostProcess(text: string, is_premium: boolean, sourceLanguage: string, language: string): string {
   const smart_quotes_replaced = text?.replaceAll(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"').replaceAll("  ", " ")
-  if (is_premium) { // premium webnovels are parsed wrongly, and need to be reuploaded. for now, this is a temporary fix.
+  if (is_premium && sourceLanguage === language) { // premium webnovels are parsed wrongly, and need to be reuploaded. for now, this is a temporary fix.
+                    // at the same time, *translated* premium webnovels are parsed correctly, because cha and ellie fix the newlines.
+                    // so these should not be normalized.
     return normalizeNewlines(smart_quotes_replaced)
   }
   return smart_quotes_replaced // community webnovels have proper newlines, so no need to normalize.

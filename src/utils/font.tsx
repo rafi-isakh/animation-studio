ad346dfa0734afc629b8ee29333bbec358e31652
server <server@stelland.io>
2025-06-11 12:11:31 +0900

    Newline problems

--- src/utils/font.tsx
+++ src/utils/font.tsx
@@ -1,7 +1,11 @@
 export function textPostProcess(text: string): string {
-    return text?.replaceAll(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"').replaceAll("  ", " ").replaceAll("\n", "\n\n").replaceAll("\n\n\n", "\n\n").replaceAll("\n\n\n\n", "\n\n");
+    return replaceSingleNewlines(text?.replaceAll(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"').replaceAll("  ", " "))
   }
 
 export function replaceSmartQuotes(text: string): string {
     return text?.replaceAll(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"');
+  }
+
+  function replaceSingleNewlines(text: string): string {
+    return text.replace(/(?<!\n)\n(?!\n)/g, '\n\n');
   }
